5916e101f69f4a8ff10f36026275dd6dca762f49
Mark Sujew <mark.sujew@typefox.io>
2021-02-24 16:56:06 +0100

    Gitpodified the repo

--- core/src/bootstrap/index.ts
+++ core/src/bootstrap/index.ts
@@ -4,14 +4,12 @@ import { parseXtext } from "./xtext-parser";
 import { buildGrammar, linkGrammar } from "./ast-builder";
 import { generateParser } from "../generator/parser-generator";
 
-const input = fs.readFileSync("core/test.xtext").toString();
+const input = fs.readFileSync("test.xtext").toString();
 const result = parseXtext(input);
 
 const ast = buildGrammar(result.cst);
 linkGrammar(ast);
 
 const parser = generateParser(ast);
-console.log(parser)
-fs.mkdirsSync("core/src/gen");
-fs.writeFileSync("core/src/gen/parser.ts", parser);
-debugger;
+fs.mkdirsSync("src/gen");
+fs.writeFileSync("src/gen/parser.ts", parser);
