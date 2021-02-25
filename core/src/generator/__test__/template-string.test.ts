4f433b1cb32cf7d017d60d013fe0465718cc07c2
Daniel Dietrich <daniel.dietrich@typefox.io>
2021-02-25 19:33:41 +0100

    Added generator/template-string

---
+++ core/src/generator/__test__/template-string.test.ts
@@ -0,0 +1,104 @@
+import { s } from '../template-string';
+
+test('Should not throw when substituting null', () => {
+    expect(s`${null}`).toBe('null');
+});
+
+test('Should not throw when substituting undefined', () => {
+    expect(s`${undefined}`).toBe('undefined');
+});
+
+test('Should indent empty string', () => {
+    expect(s``).toBe('');
+});
+
+test('Should indent simple string containing NaN', () => {
+    expect(s`${NaN}`).toBe('NaN');
+});
+
+test('Should indent simple string containing double', () => {
+    expect(s`${1.0}`).toBe('1');
+});
+
+test('Should indent simple string containing string', () => {
+    expect(s`${'ok'}`).toBe('ok');
+});
+
+test('Should indent frayed strings', () => {
+    expect(frayedIndentation).toBe(expectedFrayedIndentation);
+});
+
+test('Should indent multiline string containing no empty lines', () => {
+    expect(trimmedMultilineString).toBe(expectedTrimmedMultilineString);
+});
+
+test('Should indent multiline string containing empty lines and trailing whitespace', () => {
+    expect(multilineStringWithWhitespace).toBe(expectedMultilineStringWithWhitespace);
+});
+
+test('Should indent nested multiline strings', () => {
+    expect(generator()).toBe(expectedGeneratorOutput);
+});
+
+test('Should indent frayed multiline strings', () => {
+    expect(nestedIndentation).toBe(expectedNestedIndentation);
+});
+
+const frayedIndentation = s`
+    test1
+  test2
+`;
+
+const expectedFrayedIndentation = `  test1
+test2`;
+
+const trimmedMultilineString = s`
+  ${1}
+`;
+
+const expectedTrimmedMultilineString = '1';
+
+const multilineStringWithWhitespace = s`
+      // we add some right space
+      ${1}${'    '}
+        ${2}${'  '}
+          ${3}${''}
+    `;
+
+const expectedMultilineStringWithWhitespace = '// we add some right space\n1\n  2\n    3';
+
+const generator = () => {
+    const applyMethod = (paramName: string) => s`
+        R apply(T ${paramName});
+        boolean isDefinedAt(T ${paramName});
+    `;
+    const toStringMethod = s`
+        @Override
+        String toString() { return "T -> R"; }
+    `;
+    return s`
+        public interface PartialFunction<T, R> {
+            ${applyMethod('t')}
+            ${toStringMethod}
+        }
+    `;
+};
+
+const expectedGeneratorOutput = `public interface PartialFunction<T, R> {
+    R apply(T t);
+    boolean isDefinedAt(T t);
+    @Override
+    String toString() { return "T -> R"; }
+}`;
+
+const nestedIndentation = s`
+${1}x
+  ${s`
+      test1
+  test2
+  `}
+`;
+
+const expectedNestedIndentation = `1x
+      test1
+  test2`;
---
+++ core/src/generator/template-string.ts
@@ -0,0 +1,37 @@
+const leadingWhitespace = /^(s*\n)+/;
+const newline = /\n/g;
+const nonWhitespace = /\S|$/;
+
+/**
+ * A tag function that automatically aligns embedded multiline strings.
+ *
+ * @param staticParts the static parts of a tagged template literal
+ * @param substitutions the variable parts of a tagged template literal
+ * @returns an aligned string that consists of the given parts
+ */
+export function s(staticParts: TemplateStringsArray, ...substitutions: unknown[]): string {
+    const lines = substitutions                      // align substitutions and fuse them with static parts
+        .reduce((acc: string, subst, i) => acc + align(String(subst), acc) + staticParts[i + 1], staticParts[0])
+        .split('\n')                                 // converts text to lines
+        .map(line => line.trimRight());              // whitespace-only lines are empty (preserving leading whitespace)
+    const indent = findIndentation(lines);           // finds the minimum indentation
+    return lines
+        .map(line => line.slice(indent).trimRight()) // shifts lines to the left
+        .join('\n')                                  // convert lines to string
+        .trimRight()                                 // removes trailing whitespace after joining possibly empty lines
+        .replace(leadingWhitespace, '');             // removes leading whitespace while preserving indentation
+}
+
+// add the alignment of the previous static part to all lines of the following substitution
+function align(subst: string, acc: string): string {
+    const length = Math.max(0, acc.length - acc.lastIndexOf('\n') - 1);
+    const indent = ' '.repeat(length);
+    return subst.replace(newline, '\n' + indent);
+}
+
+// finds the indentation of a text block represented by a sequence of lines
+function findIndentation(lines: string[]): number {
+    const indents = lines.filter(line => line.length > 0).map(line => line.search(nonWhitespace));
+    const min = indents.length === 0 ? 0 : Math.min(...indents); // min(...[]) = min() = Infinity
+    return Math.max(0, min);
+}
