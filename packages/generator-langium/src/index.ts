29d4d281ab2b411178926011e3a98c8ea4602ba4
Pluralia <irina.artemeva@typefox.io>
2021-06-10 15:05:52 +0200

    Remove yeoman experiments

--- packages/generator-langium/src/index.ts
+++ packages/generator-langium/src/index.ts
@@ -10,81 +10,37 @@ class LangiumGenerator extends Generator
         super(args, options);
         
         // USE: template context
-        // this.log("Source root", this.sourceRoot());
-        this.sourceRoot("template-files");
-        // this.log("Updated source root", this.sourceRoot());
-
-        // USE: destination context
-        // this.log("Destination root", this.destinationRoot());
-
-        // USE: compose with another generator
-        // this.composeWith(require.resolve('generator-code/generators/app'));
- 
-        // USE: arguments
-        // this.argument("age", { type: Number, required: false });
-        // const age = this.options.age;
-        // if (age != undefined) {
-        //   this.log(age);
-        // }
-
-        // USE: options
-        // this.option("nice", { type: Boolean, alias: "n", description: "is weather nice?" });
-        // const nice = this.options.nice ? "YES" : "NO";
-        // this.log("Is option here?", nice);
+        this.sourceRoot("langium-template");
+        this.log("Updated source root: ", this.sourceRoot());
      }
 
     initialize(): void {
         this.log('initialize()');
-
-        // XXX
-        // this.prompting().then(_ => {
-        //     this.log("It's okay")
-        // });
-    }
-
-    install(): void {
-      this.log('install()');
-      this.spawnCommand('npm', ['install', 'chevrotain']);
     }
 
     async prompting() {
         this.log('prompting()');
-          this.answers = await this.prompt([ 
+        this.answers = await this.prompt([ 
           {
             type: "input",
-            name: "projName",
+            name: "project_name",
             message: "Your project name",
-            default: "langium-hello-world",
-            // USE: keep answer
-            // store: true
-          },
-          // USE: confirmation
-          // {
-          //   type: "confirm",
-          //   name: "cool",
-          //   message: "Would you like to enable the Cool feature?"
-          // }
+            default: "langium-hello-world"
+          }
         ]);
-      }
-
-    writing(): void {
-        this.log("WRITING: app name", this.answers.projName);
-
-        // USE: make files from the template directory
-        this.fs.copyTpl(
-          this.templatePath('index.html'),
-          this.destinationPath(this.answers.projName, 'index.html'),
-            { title: 'Templating with Yeoman' }
-        );  
     }
 
-    // install(): void {
-    //     this.log('install() : Where installation are run (npm, bower)');
-    // }
+    writing(): void {
+        this.log("writing()", this.answers.project_name);
+    }
 
-    // end(): void {
-    //     this.log('end() : Called last, cleanup, say good bye, etc');
-    // }
+    install(): void {
+        this.log('install()');
+    }
+
+    end(): void {
+        this.log("Have a nice coding :)");
+    }
 }
 
 export = LangiumGenerator;
