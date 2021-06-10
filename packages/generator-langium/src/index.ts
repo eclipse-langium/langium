d981a0e03397ed7def0a26cdbd9fb9039d66b7f6
Pluralia <irina.artemeva@typefox.io>
2021-06-10 16:11:38 +0200

    Add Langium wizard

--- packages/generator-langium/src/index.ts
+++ packages/generator-langium/src/index.ts
@@ -1,44 +1,74 @@
-import Generator from  'yeoman-generator';
+import Generator       from  'yeoman-generator';
+import { StringUtils } from 'turbocommons-ts';
 
 // FIX: get during running: "No change to package.json was detected. No package manager install will be executed."
+
+const TEMPLATE_DIR   = "langium-template";
+const USER_DIR       = "app";
+const EXTENSION_NAME = "extension-name";
+const LANGUAGE_ID    = "language-id";
+const LANGUAGE_NAME  = "LanguageName";
 
 class LangiumGenerator extends Generator
 {
     // FIX: type of answers
     private answers: any;
+
     constructor(args: string | string[], options: Generator.GeneratorOptions) {
         super(args, options);
-        
-        // USE: template context
-        this.sourceRoot("langium-template");
-        this.log("Updated source root: ", this.sourceRoot());
-     }
-
-    initialize(): void {
-        this.log('initialize()');
+        this.sourceRoot(TEMPLATE_DIR);
     }
 
     async prompting() {
-        this.log('prompting()');
         this.answers = await this.prompt([ 
           {
             type: "input",
-            name: "project_name",
-            message: "Your project name",
-            default: "langium-hello-world"
+            name: "extension_name",
+            message: "Your extension name",
+            default: EXTENSION_NAME
+          },
+          {
+            type: "input",
+            name: "language_id",
+            message: "Your language identifier",
+            default: LANGUAGE_ID
           }
         ]);
     }
 
-    writing(): void {
-        this.log("writing()", this.answers.project_name);
+    _ifLanguageIdCorrect(): boolean {
+          // FIX: is it nice identifier?
+        return new RegExp('^[a-zA-Z0-9\-]+$').test(this.answers.language_id.toString());          
     }
 
-    install(): void {
-        this.log('install()');
+    _toCamelCase(input: string): string {
+        return StringUtils.formatCase(input, StringUtils.FORMAT_UPPER_CAMEL_CASE);
+    }
+
+    // FIX: types
+    _replaceInTemplate(answers: any, content: any): string {
+        // FIX: regex can be replaced on parsers, but for what?
+        return content.toString()
+          .replace(new RegExp(EXTENSION_NAME, 'g'), answers.extension_name)
+          .replace(new RegExp(LANGUAGE_ID, 'g'), answers.language_id)
+          .replace(new RegExp(LANGUAGE_NAME, 'g'), this._toCamelCase(answers.language_id));
+      }
+
+    writing(): void {
+        if (!this._ifLanguageIdCorrect()) {
+          this.log("A language identifier can be NOT CORRECT ID");
+        } else {
+          this.fs.copy(
+            this.templatePath(EXTENSION_NAME, '.'),
+            this.destinationPath(USER_DIR, this.answers.extension_name),
+            { process: x => this._replaceInTemplate(this.answers, x) }
+          );
+        }
     }
 
     end(): void {
+        this.log("Extension name:", this.answers.extension_name);
+        this.log("Language identifier:", this.answers.language_id);
         this.log("Have a nice coding :)");
     }
 }
