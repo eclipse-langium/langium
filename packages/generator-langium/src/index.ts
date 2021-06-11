3ba94092c50dc87b03dc5eac01af62d6d2124a22
Pluralia <irina.artemeva@typefox.io>
2021-06-11 14:29:07 +0200

    Small fixes

--- packages/generator-langium/src/index.ts
+++ packages/generator-langium/src/index.ts
@@ -3,11 +3,11 @@ import { StringUtils } from 'turbocommons-ts';
 
 // FIX: get during running: "No change to package.json was detected. No package manager install will be executed."
 
-const TEMPLATE_DIR   = "langium-template";
-const USER_DIR       = "app";
-const EXTENSION_NAME = "extension-name";
-const LANGUAGE_ID    = "language-id";
-const LANGUAGE_NAME  = "LanguageName";
+const TEMPLATE_DIR   = 'langium-template';
+const USER_DIR       = 'app';
+const EXTENSION_NAME = 'extension-name';
+const LANGUAGE_ID    = 'language-id';
+const LANGUAGE_NAME  = 'LanguageName';
 
 class LangiumGenerator extends Generator
 {
@@ -19,7 +19,7 @@ class LangiumGenerator extends Generator
         this.sourceRoot(TEMPLATE_DIR);
     }
 
-    async prompting() {
+    async prompting(): Promise<void> {
         this.answers = await this.prompt([ 
           {
             type: "input",
@@ -31,22 +31,22 @@ class LangiumGenerator extends Generator
             type: "input",
             name: "language_id",
             message: "Your language identifier",
-            default: LANGUAGE_ID
+            default: LANGUAGE_ID,
+            validate: function(input: string): boolean | string {
+                if (/^[a-zA-Z_][\w]+$/.test(input.toString()))
+                   return true;
+                return "You entered not correct language-id. Try again.";
+            }
           }
         ]);
     }
 
-    _ifLanguageIdCorrect(): boolean {
-          // FIX: is it nice identifier?
-        return new RegExp('^[a-zA-Z0-9\-]+$').test(this.answers.language_id.toString());          
-    }
-
-    _toCamelCase(input: string): string {
+    private _toCamelCase(input: string): string {
         return StringUtils.formatCase(input, StringUtils.FORMAT_UPPER_CAMEL_CASE);
     }
 
     // FIX: types
-    _replaceInTemplate(answers: any, content: any): string {
+    private _replaceInTemplate(answers: any, content: any): string {
         // FIX: regex can be replaced on parsers, but for what?
         return content.toString()
           .replace(new RegExp(EXTENSION_NAME, 'g'), answers.extension_name)
@@ -55,15 +55,11 @@ class LangiumGenerator extends Generator
       }
 
     writing(): void {
-        if (!this._ifLanguageIdCorrect()) {
-          this.log("A language identifier can be NOT CORRECT ID");
-        } else {
-          this.fs.copy(
-            this.templatePath(EXTENSION_NAME, '.'),
-            this.destinationPath(USER_DIR, this.answers.extension_name),
-            { process: x => this._replaceInTemplate(this.answers, x) }
-          );
-        }
+        this.fs.copy(
+          this.templatePath(EXTENSION_NAME, '.'),
+          this.destinationPath(USER_DIR, this.answers.extension_name),
+          { process: x => this._replaceInTemplate(this.answers, x) }
+        );
     }
 
     end(): void {
