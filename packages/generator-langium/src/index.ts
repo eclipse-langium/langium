3fd23e8b5c0ed752655d41a5d30726953cfff5a4
Pluralia <irina.artemeva@typefox.io>
2021-06-13 21:41:31 +0200

    Refactoring

--- packages/generator-langium/src/index.ts
+++ packages/generator-langium/src/index.ts
@@ -1,7 +1,6 @@
 import Generator      from 'yeoman-generator';
 import *         as _ from 'lodash';
 
-// FIX: get during running: "No change to package.json was detected. No package manager install will be executed."
 
 const TEMPLATE_DIR   = 'langium-template';
 const USER_DIR       = 'app';
@@ -16,8 +15,12 @@ const LANGUAGE_ID    = 'language-id';
 
 class LangiumGenerator extends Generator
 {
-    // FIX: type of answers
-    private answers: any;
+    private answers: {
+      extensionName: string,
+      languageName: string,
+      fileExtension: string,
+      languageId: string
+    };
 
     constructor(args: string | string[], options: Generator.GeneratorOptions) {
         super(args, options);
@@ -45,7 +48,7 @@ class LangiumGenerator extends Generator
             message: "File extension of your language",
             default: FILE_EXTENSION,
             validate: (input: string): boolean | string =>
-                (/^[a-z]*$/.test(input.toString())) ? true : "Extension can contain only small letters. Try again."
+              /^[a-z]*$/.test(input) ? true : "Extension can contain only small letters. Try again."
           }
         ]);
     }
@@ -54,24 +57,21 @@ class LangiumGenerator extends Generator
         this.answers.languageName = _.upperFirst(_.camelCase(this.answers.languageName
           .replace(/[ -]+/g, '_')));
         this.answers.languageId = _.snakeCase(this.answers.languageName);
-  
+
         this.sourceRoot(TEMPLATE_DIR);
         [".", ".vscode", ".eslintrc.json", ".vscodeignore"].map(path => {
-          const replaceTemplateWords = function(answers: any, content: Buffer): string {
-            // FIX: regex can be replaced on parsers, but for what?
-            const replaceMap = [ [EXTENSION_NAME, answers.extensionName]
-                               , [FILE_EXTENSION, answers.fileExtension]
-                               , [LANGUAGE_ID, answers.languageId]
-                               , [LANGUAGE_NAME, answers.languageName]];
-            return replaceMap.reduce(
-                (acc: string, [templateWord, userAnswer]) => acc.replace(new RegExp(`${OPEN}${templateWord}${CLOSE}`, 'g'), userAnswer)
-              , content.toString());
-          }
+          const replaceTemplateWords = (answers: any, content: Buffer): string =>
+            [ [EXTENSION_NAME, answers.extensionName]
+            , [FILE_EXTENSION, answers.fileExtension]
+            , [LANGUAGE_ID, answers.languageId]
+            , [LANGUAGE_NAME, answers.languageName] ]
+              .reduce(
+                  (acc: string, [templateWord, userAnswer]) => acc.replace(new RegExp(`${OPEN}${templateWord}${CLOSE}`, 'g'), userAnswer)
+                , content.toString());
 
-          const replaceTemplateNames = function(answers: any, path: string): string {
-            return path
+          const replaceTemplateNames = (answers: any, path: string): string =>
+            path
               .replace(new RegExp(LANGUAGE_ID, 'g'), answers.languageId);
-          }
 
           this.fs.copy(
             this.templatePath(path),
@@ -83,10 +83,10 @@ class LangiumGenerator extends Generator
     }
 
     end(): void {
-        this.log("Extension name:", this.answers.extensionName);
-        this.log("Language name:", this.answers.languageName);
+        this.log("Extension name:",      this.answers.extensionName);
+        this.log("Language name:",       this.answers.languageName);
         this.log("Language identifier:", this.answers.languageId);
-        this.log("File extension:", this.answers.fileExtension);
+        this.log("File extension:",      this.answers.fileExtension);
         this.log("Have a nice coding :)");
     }
 }
