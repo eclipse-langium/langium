89f039e0afbf615fa813a41eee2f31442498ba24
Mark Sujew <mark.sujew@typefox.io>
2021-06-15 15:43:31 +0200

    Add `getNameNode` to `NameProvider`

--- packages/langium/src/references/naming.ts
+++ packages/langium/src/references/naming.ts
@@ -1,4 +1,5 @@
-import { AstNode } from '../syntax-tree';
+import { findNodeForFeature } from '../grammar/grammar-util';
+import { AstNode, CstNode } from '../syntax-tree';
 
 export interface NamedAstNode extends AstNode {
     name: string;
@@ -10,6 +11,11 @@ export function isNamed(node: AstNode): node is NamedAstNode {
 
 export interface NameProvider {
     getName(node: AstNode): string | undefined;
+    /**
+     * Returns the `CstNode` which contains the parsed value of the `name` assignment.
+     * @param node Specified `AstNode` whose name node shall be retrieved.
+     */
+    getNameNode(node: AstNode): CstNode | undefined;
 }
 
 export class DefaultNameProvider implements NameProvider {
@@ -18,5 +24,9 @@ export class DefaultNameProvider implements NameProvider {
             return node.name;
         }
         return undefined;
+    }
+
+    getNameNode(node: AstNode): CstNode | undefined {
+        return findNodeForFeature(node.$cstNode, 'name');
     }
 }
