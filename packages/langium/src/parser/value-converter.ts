e0a0945b6a16ccf442485a999939a95fbbb52f7c
Mark Sujew <mark.sujew@typefox.io>
2021-06-17 12:38:18 +0200

    Add license header

--- packages/langium/src/parser/value-converter.ts
+++ packages/langium/src/parser/value-converter.ts
@@ -1,3 +1,9 @@
+/******************************************************************************
+ * Copyright 2021 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
 import { isRuleCall } from '../grammar/generated/ast';
 import { CstNode } from '../syntax-tree';
 
--- packages/langium/src/service/symbols/document-symbol-provider.ts
+++ packages/langium/src/service/symbols/document-symbol-provider.ts
@@ -1,3 +1,9 @@
+/******************************************************************************
+ * Copyright 2021 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
 import { DocumentSymbol, SymbolKind } from 'vscode-languageserver';
 import { LangiumDocument } from '../../documents/document';
 import { NameProvider } from '../../references/naming';
