43306903d01e110c64b91e192909df20e32f9f05
Mark Sujew <mark.sujew@typefox.io>
2021-06-25 11:32:06 +0200

    Rewrite AST generator to new API

--- packages/langium-cli/src/generator/ast-generator.ts
+++ packages/langium-cli/src/generator/ast-generator.ts
@@ -12,27 +12,29 @@ import { generatedHeader } from './util';
 export function generateAst(grammar: Grammar, config: LangiumConfig): string {
     const types = collectAst(grammar);
     const fileNode = new CompositeGeneratorNode();
-    fileNode.children.push(
+    fileNode.append(
         generatedHeader,
         '/* eslint-disable @typescript-eslint/array-type */', NL,
         '/* eslint-disable @typescript-eslint/no-empty-interface */', NL,
     );
     const crossRef = hasCrossReferences(grammar);
     if (config.langiumInternal) {
-        fileNode.children.push(`import { AstNode, AstReflection${crossRef ? ', Reference' : ''} } from '../../syntax-tree';`, NL);
-        fileNode.children.push("import { isAstNode } from '../../utils/ast-util';", NL, NL);
+        fileNode.append(
+            `import { AstNode, AstReflection${crossRef ? ', Reference' : ''} } from '../../syntax-tree';`, NL,
+            "import { isAstNode } from '../../utils/ast-util';", NL, NL
+        );
     } else {
-        fileNode.children.push(`import { AstNode, AstReflection${crossRef ? ', Reference' : ''}, isAstNode } from 'langium';`, NL, NL);
+        fileNode.append(`import { AstNode, AstReflection${crossRef ? ', Reference' : ''}, isAstNode } from 'langium';`, NL, NL);
     }
 
     for (const type of types) {
-        fileNode.children.push(type.toString(), NL);
+        fileNode.append(type.toString(), NL);
     }
     for (const primitiveRule of stream(grammar.rules).filterType(isParserRule).filter(e => isDataTypeRule(e))) {
-        fileNode.children.push(buildDatatype(primitiveRule), NL, NL);
+        fileNode.append(buildDatatype(primitiveRule), NL, NL);
     }
 
-    fileNode.children.push(generateAstReflection(grammar, types));
+    fileNode.append(generateAstReflection(grammar, types));
 
     return processNode(fileNode);
 }
@@ -64,52 +66,62 @@ type CrossReferenceType = {
 function generateAstReflection(grammar: Grammar, interfaces: Interface[]): GeneratorNode {
     const reflectionNode = new CompositeGeneratorNode();
     const crossReferenceTypes = buildCrossReferenceTypes(interfaces);
-    reflectionNode.children.push(
+
+    reflectionNode.append(
         'export type ', grammar.name, 'AstType = ',
         interfaces.map(t => `'${t.name}'`).join(' | '),
-        ';', NL, NL
-    );
-    reflectionNode.children.push(
+        ';', NL, NL,
         'export type ', grammar.name, 'AstReference = ',
         crossReferenceTypes.map(e => `'${e.type}:${e.feature}'`).join(' | ') || 'never',
-        ';', NL, NL
+        ';', NL, NL,
+        'export class ', grammar.name, 'AstReflection implements AstReflection {', NL, NL
     );
-    reflectionNode.children.push('export class ', grammar.name, 'AstReflection implements AstReflection {', NL, NL);
 
-    const classBodyNode = new IndentNode();
-    classBodyNode.children.push('getAllTypes(): string[] {', NL);
-    const allTypesNode = new IndentNode();
-    allTypesNode.children.push('return [', interfaces.map(t => `'${t.name}'`).join(', '), '];', NL);
-    classBodyNode.children.push(allTypesNode, '}', NL, NL);
-    classBodyNode.children.push('isInstance(node: unknown, type: string): boolean {', NL);
-    const isInstanceNode = new IndentNode();
-    isInstanceNode.children.push('return isAstNode(node) && this.isSubtype(node.$type, type);', NL);
-    classBodyNode.children.push(isInstanceNode, '}', NL, NL);
-    classBodyNode.children.push('isSubtype(subtype: string, supertype: string): boolean {', NL);
-    classBodyNode.children.push(buildIsSubtypeMethod(interfaces), '}', NL, NL);
-    classBodyNode.children.push('getReferenceType(referenceId: ', grammar.name, 'AstReference): string {', NL);
-    classBodyNode.children.push(buildReferenceTypeMethod(interfaces), '}', NL);
-    reflectionNode.children.push(classBodyNode, '}', NL, NL);
-    reflectionNode.children.push('export const reflection = new ', grammar.name, 'AstReflection();', NL);
+    reflectionNode.indent(classBody => {
+        classBody.append('getAllTypes(): string[] {', NL);
+        classBody.indent(allTypes => {
+            allTypes.append('return [', interfaces.map(t => `'${t.name}'`).join(', '), '];', NL);
+        });
+        classBody.append('isInstance(node: unknown, type: string): boolean {', NL);
+        classBody.indent(isInstance => {
+            isInstance.append('return isAstNode(node) && this.isSubtype(node.$type, type);', NL);
+        });
+        classBody.append(
+            '}', NL, NL,
+            'isSubtype(subtype: string, supertype: string): boolean {', NL,
+            buildIsSubtypeMethod(interfaces), '}', NL, NL,
+            'getReferenceType(referenceId: ', grammar.name, 'AstReference): string {', NL,
+            buildReferenceTypeMethod(interfaces), '}', NL,
+        );
+    });
+
+    reflectionNode.append(
+        '}', NL, NL,
+        'export const reflection = new ', grammar.name, 'AstReflection();', NL
+    );
+
     return reflectionNode;
 }
 
 function buildReferenceTypeMethod(interfaces: Interface[]): GeneratorNode {
     const crossReferenceTypes = buildCrossReferenceTypes(interfaces);
     const typeSwitchNode = new IndentNode();
-    typeSwitchNode.children.push('switch (referenceId) {', NL);
-    const caseNode = new IndentNode();
-    for (const crossRef of crossReferenceTypes) {
-        caseNode.children.push(`case '${crossRef.type}:${crossRef.feature}': {`, NL);
-        const caseContentNode = new IndentNode();
-        caseContentNode.children.push(`return ${crossRef.referenceType};`, NL);
-        caseNode.children.push(caseContentNode, '}', NL);
-    }
-    caseNode.children.push('default: {', NL);
-    const defaultNode = new IndentNode();
-    defaultNode.children.push('throw new Error(`${referenceId} is not a valid reference id.`);', NL);
-    caseNode.children.push(defaultNode, '}', NL);
-    typeSwitchNode.children.push(caseNode, '}', NL);
+    typeSwitchNode.append('switch (referenceId) {', NL);
+    typeSwitchNode.indent(caseNode => {
+        for (const crossRef of crossReferenceTypes) {
+            caseNode.append(`case '${crossRef.type}:${crossRef.feature}': {`, NL);
+            caseNode.indent(caseContent => {
+                caseContent.append(`return ${crossRef.referenceType};`, NL);
+            });
+            caseNode.append('}', NL);
+        }
+        caseNode.append('default: {', NL);
+        caseNode.indent(defaultNode => {
+            defaultNode.append('throw new Error(`${referenceId} is not a valid reference id.`);', NL);
+        });
+        caseNode.append('}', NL);
+    });
+    typeSwitchNode.append('}', NL);
     return typeSwitchNode;
 }
 
@@ -125,30 +137,36 @@ function buildCrossReferenceTypes(interfaces: Interface[]): CrossReferenceType[]
 
 function buildIsSubtypeMethod(interfaces: Interface[]): GeneratorNode {
     const methodNode = new IndentNode();
-    methodNode.children.push('if (subtype === supertype) {', NL);
-    const ifNode = new IndentNode();
-    ifNode.children.push('return true;', NL);
-    methodNode.children.push(ifNode, '}', NL);
-    methodNode.children.push('switch (subtype) {', NL);
-    const switchNode = new IndentNode();
-    const groups = groupBySupertypes(interfaces.filter(e => e.superTypes.length > 0));
+    methodNode.append('if (subtype === supertype) {', NL);
+    methodNode.indent(ifNode => {
+        ifNode.append('return true;', NL);
+    });
+    methodNode.append(
+        '}', NL,
+        'switch (subtype) {', NL
+    );
+    methodNode.indent(switchNode => {
+        const groups = groupBySupertypes(interfaces.filter(e => e.superTypes.length > 0));
 
-    for (const [superTypes, typeGroup] of groups.entries()) {
-        for (const typeItem of typeGroup) {
-            switchNode.children.push(`case ${typeItem.name}:`, NL);
+        for (const [superTypes, typeGroup] of groups.entries()) {
+            for (const typeItem of typeGroup) {
+                switchNode.append(`case ${typeItem.name}:`, NL);
+            }
+            switchNode.children.pop();
+            switchNode.append(' {', NL);
+            switchNode.indent(caseNode => {
+                caseNode.append('return ', superTypes.split(':').map(e => `this.isSubtype(${e}, supertype)`).join(' || '), ';');
+            });
+            switchNode.append(NL, '}', NL);
         }
-        switchNode.children.pop();
-        switchNode.children.push(' {', NL);
-        const caseNode = new IndentNode();
-        caseNode.children.push('return ', superTypes.split(':').map(e => `this.isSubtype(${e}, supertype)`).join(' || '), ';');
-        switchNode.children.push(caseNode, NL, '}', NL);
-    }
 
-    switchNode.children.push('default: {', NL);
-    const defaultNode = new IndentNode();
-    defaultNode.children.push('return false;', NL);
-    switchNode.children.push(defaultNode, '}', NL);
-    methodNode.children.push(switchNode, '}', NL);
+        switchNode.append('default: {', NL);
+        switchNode.indent(defaultNode => {
+            defaultNode.append('return false;', NL);
+        });
+        switchNode.append('}', NL);
+    });
+    methodNode.append('}', NL);
     return methodNode;
 }
 
