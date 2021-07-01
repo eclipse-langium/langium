5ff3b9f3f0a5e55bd9f5b9a7d8dab3c3f54ef093
Mark Sujew <mark.sujew@typefox.io>
2021-07-01 14:42:23 +0000

    Removed copyfiles dev dependency

--- packages/langium-cli/src/generator/grammar-serializer.ts
+++ packages/langium-cli/src/generator/grammar-serializer.ts
@@ -1,19 +1,31 @@
-import { CompositeGeneratorNode, Grammar, LangiumServices, NL, process } from 'langium';
+/******************************************************************************
+ * Copyright 2021 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { CompositeGeneratorNode, Grammar, LangiumServices, NL, processGeneratorNode } from 'langium';
 import { LangiumConfig } from '../package';
 import { generatedHeader } from './util';
 
 export function serializeGrammar(services: LangiumServices, grammar: Grammar, config: LangiumConfig): string {
     const json = services.serializer.JsonSerializer.serialize(grammar, 2);
     const node = new CompositeGeneratorNode();
-    node.children.push(generatedHeader);
+    node.append(generatedHeader);
 
     if (config.langiumInternal) {
-        node.children.push("import { loadGrammar } from '../grammar-util';", NL);
-        node.children.push("import { Grammar } from './ast';", NL, NL);
+        node.append(
+            "import { loadGrammar } from '../grammar-util';", NL,
+            "import { Grammar } from './ast';"
+        );
     } else {
-        node.children.push("import { loadGrammar, Grammar } from 'langium';", NL, NL);
+        node.append("import { loadGrammar, Grammar } from 'langium';");
     }
+    node.append(NL, NL);
 
-    node.children.push('const grammar = (): Grammar => loadGrammar(`', json.replace(/\\/g, '\\\\'), '`);', NL, NL, 'export default grammar;');
-    return process(node);
+    node.append(
+        'const grammar = (): Grammar => loadGrammar(`', json.replace(/\\/g, '\\\\'), '`);', NL, NL,
+        'export default grammar;', NL
+    );
+    return processGeneratorNode(node);
 }
