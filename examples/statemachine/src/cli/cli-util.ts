971cfa25197891b982ee192f676f722ae613171a
Pluralia <irina.artemeva@typefox.io>
2021-09-15 18:05:28 +0200

    Update cli-util

--- examples/statemachine/src/cli/cli-util.ts
+++ examples/statemachine/src/cli/cli-util.ts
@@ -6,8 +6,9 @@
 
 import fs from 'fs';
 import colors from 'colors';
-import { AstNode, LangiumDocument, LangiumDocumentConfiguration, LangiumServices } from 'langium';
+import { AstNode, LangiumDocument, LangiumServices } from 'langium';
 import path from 'path';
+import { createStatemachineServices } from '../language-server/statemachine-module';
 
 export function extractDocument(fileName: string, languageId: string, extensions: string[], services: LangiumServices): LangiumDocument {
     if (!extensions.includes(path.extname(fileName))) {
@@ -19,9 +20,9 @@ export function extractDocument(fileName: string, languageId: string, extensions
         console.error(colors.red(`File ${fileName} doesn't exist.`));
         process.exit(1);
     }
-    const fileContent = fs.readFileSync(fileName, 'utf-8');
 
-    const document = LangiumDocumentConfiguration.create(`file:${fileName}`, languageId, 0, fileContent);
+    const domainModelServices = createStatemachineServices();
+    const document = domainModelServices.documents.LangiumDocuments.getOrCreateDocument(path.resolve(fileName));
 
     const buildResult = services.documents.DocumentBuilder.build(document);
     const validationErrors = buildResult.diagnostics.filter(e => e.severity === 1);
@@ -29,7 +30,7 @@ export function extractDocument(fileName: string, languageId: string, extensions
         console.error(colors.red('There are validation errors:'));
         for (const validationError of validationErrors) {
             console.error(colors.red(
-                `line ${validationError.range.start.line}: ${validationError.message} [${document.getText(validationError.range)}]`
+                `line ${validationError.range.start.line}: ${validationError.message} [${document.textDocument.getText(validationError.range)}]`
             ));
         }
         process.exit(1);
