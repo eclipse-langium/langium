16a8206e5474596a6bb6ef0aae0043c1852d0a4f
Guillaume Fontorbe <guillaume.fontorbe@typefox.io>
2022-02-11 10:13:37 +0100

    Removed trailing spaces

--- packages/langium/src/serializer/json-serializer.ts
+++ packages/langium/src/serializer/json-serializer.ts
@@ -11,8 +11,8 @@ import { isAstNode, isReference } from '../utils/ast-util';
 
 export interface JsonSerializer {
     /**
-     * Serialize an `AstNode` into a JSON `string`. 
-     * @param node The `AstNode` to be serialized
+     * Serialize an `AstNode` into a JSON `string`.
+     * @param node The `AstNode` to be serialized.
      * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
      */
     serialize(node: AstNode, space?: string | number): string
