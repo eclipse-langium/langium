8fc9e5c21d8105fe028dda6777cd86b07182340a
Daniel Dietrich <cafebab3@gmail.com>
2022-02-18 16:57:56 +0100

    Added type testing using tsd

---
+++ packages/langium/test-d/dependency-injection.test-d.ts
@@ -0,0 +1,8 @@
+/******************************************************************************
+ * Copyright 2021 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+import { inject } from './dependency-injection';
+
+inject({});
