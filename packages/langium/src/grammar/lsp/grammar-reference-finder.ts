9a219df9fd16ce03f99e837ac8b9333d8c941043
Guillaume Fontorbe <guillaume.fontorbe@typefox.io>
2022-06-23 16:11:08 +0200

    Refactored reference finder

--- packages/langium/src/grammar/lsp/grammar-reference-finder.ts
+++ packages/langium/src/grammar/lsp/grammar-reference-finder.ts
@@ -1,20 +1,20 @@
 /******************************************************************************
- * Copyright 2021 TypeFox GmbH
- * This program and the accompanying materials are made available under the
- * terms of the MIT License, which is available in the project root.
- ******************************************************************************/
+* Copyright 2021 TypeFox GmbH
+* This program and the accompanying materials are made available under the
+* terms of the MIT License, which is available in the project root.
+******************************************************************************/
 
 import { Location, ReferenceParams } from 'vscode-languageserver';
 import { DefaultReferenceFinder } from '../../lsp';
+import { References } from '../../references/references';
 import { LangiumServices } from '../../services';
 import { AstNode, LeafCstNode } from '../../syntax-tree';
 import { extractAssignments, extractRootNode, getContainerOfType } from '../../utils/ast-util';
 import { findLeafNodeAtOffset } from '../../utils/cst-util';
-import { ReferenceDescription } from '../../workspace/ast-descriptions';
 import { AstNodeLocator } from '../../workspace/ast-node-locator';
 import { LangiumDocument, LangiumDocuments } from '../../workspace/documents';
-import { AbstractType, Action, Assignment, Interface, isAction, isAssignment, isGroup, isInterface, isParserRule, isType, isTypeAttribute, ParserRule, TypeAttribute } from '../generated/ast';
-import { collectChildrenTypes, collectSuperTypes } from '../type-system/types-util';
+import { Action, Assignment, Interface, isAction, isAssignment, isGroup, isInterface, isParserRule, isTypeAttribute, ParserRule, Type, TypeAttribute } from '../generated/ast';
+import { collectChildrenTypes, collectSuperTypes, findAttributeLeafNodeInInterface } from '../type-system/types-util';
 
 export class LangiumGrammarReferenceFinder extends DefaultReferenceFinder {
     readonly astNodeLocator: AstNodeLocator;
@@ -49,110 +49,78 @@ export class LangiumGrammarReferenceFinder extends DefaultReferenceFinder {
         const refs: Location[] = [];
         const parserRule = getContainerOfType(targetAstNode, isParserRule);
         const groupNode = getContainerOfType(targetAstNode, isGroup);
+        const interfaces: Interface[] = [];
 
         if (parserRule?.returnType?.ref) {
-            const interfaces = this.getInterfacesFromAssignment(parserRule.returnType.ref);
-            interfaces.forEach(interfaceNode => {
-                const leafNode = this.findLeafNodeInInterface(interfaceNode,selectedNode.text);
-                if (leafNode) {
-                    refs.push(...this.getReferences(leafNode, params, document));
-                }
-            });
+            interfaces.push(...collectSuperTypes(parserRule.returnType.ref as Interface | Type));
         }
-
         if (groupNode) {
-            const action = groupNode.elements.find(el => isAction(el));
-            if (isAction(action)) {
-                const ref = action.type?.ref;
-                if (ref) {
-                    const interfaces = this.getInterfacesFromAssignment(ref);
-                    interfaces.forEach(interfaceNode => {
-                        const leafNode = this.findLeafNodeInInterface(interfaceNode,selectedNode.text);
-                        if (leafNode) {
-                            refs.push(...this.getReferences(leafNode, params, document));
-                        }
-                    });
-                }
-
+            const action = groupNode.elements.find(el => isAction(el)) as Action | undefined;
+            if (action?.type?.ref) {
+                interfaces.push(...collectSuperTypes(action.type.ref as Interface | Type));
             }
         }
-
+        for (const interf of interfaces) {
+            const leaf = findAttributeLeafNodeInInterface(interf, selectedNode.text);
+            if (leaf) {
+                refs.push(...this.getReferences(leaf, params, document));
+                break;
+            }
+        }
         return refs;
-    }
-
-    findLeafNodeInInterface(interfaceNode: Interface, text: string): LeafCstNode | undefined {
-        const attribute = interfaceNode.attributes.find(a => a.name === text);
-        if (attribute) {
-            const cstNode = attribute.$cstNode;
-            if (cstNode) {
-                const leafNode = findLeafNodeAtOffset(cstNode, cstNode.offset);
-                if (leafNode) {
-                    return leafNode;
-                }
-            }
-        }
-        return undefined;
-    }
-
-    getInterfacesFromAssignment(ref: AbstractType): Set<Interface> {
-        let interfaces: Set<Interface> = new Set<Interface>();
-        if (isInterface(ref)) {
-            interfaces = collectSuperTypes(ref);
-            interfaces.add(ref);
-        } else if (isType(ref)) {
-            interfaces = collectSuperTypes(ref);
-        }
-        return interfaces;
     }
 
     getReferencesFromTypeAttribute(typeAttributeNode: TypeAttribute, selectedNode: LeafCstNode): Location[] {
         const refs: Location[] = [];
         const interfaceNode = getContainerOfType(typeAttributeNode, isInterface);
         if (interfaceNode) {
-            const collectedTypes = collectChildrenTypes(interfaceNode, this.references, this.langiumDocuments, this.astNodeLocator);
-            collectedTypes.add(interfaceNode);
+            const interfaces = collectChildrenTypes(interfaceNode, this.references, this.langiumDocuments, this.astNodeLocator);
 
-            const referencesToTypes = new Set<ReferenceDescription>();
-            collectedTypes.forEach(collectedType => {
-                const refs = this.references.findReferences(collectedType);
-                for (const ref of refs) {
-                    referencesToTypes.add(ref);
-                }
+            const targetRules: Array<ParserRule | Action> = [];
+            interfaces.forEach(interf => {
+                targetRules.push(...getParserRulesAndActionsWithReturnType(interf, this.references, this.langiumDocuments, this.astNodeLocator));
             });
 
-            const parserRules = this.findParserRulesFromReferences(referencesToTypes);
-
-            parserRules.forEach(rule => {
+            targetRules.forEach(rule => {
                 const assignment = isParserRule(rule) ?
-                    extractAssignments(rule.definition).find(assignment => assignment.feature === selectedNode.text)
-                    : extractAssignments(getContainerOfType(rule, isGroup)!).find(assignment => assignment.feature === selectedNode.text);
+                    extractAssignments(rule.definition).find(assignment => assignment.feature === selectedNode.text) :
+                    extractAssignments(getContainerOfType(rule, isGroup)!).find(assignment => assignment.feature === selectedNode.text);
 
                 if (assignment) {
-                    const cstNode = assignment.$cstNode;
-                    if (cstNode) {
-                        const leaf = findLeafNodeAtOffset(cstNode, cstNode.offset);
-                        if (leaf) {
-                            const rootNode = extractRootNode(leaf.element);
-                            if (rootNode) {
-                                refs.push(Location.create(rootNode.$document!.uri.toString(), leaf.range));
-                            }
-                        }
+                    const location = getLocationOfAssignment(assignment);
+                    if (location) {
+                        refs.push(location);
                     }
                 }
             });
         }
         return refs;
     }
-
-    findParserRulesFromReferences(referencesToTypes: Set<ReferenceDescription>): Set<ParserRule | Action> {
-        const parserRules: Set<ParserRule | Action> = new Set<ParserRule | Action>();
-        referencesToTypes.forEach(ref => {
-            const doc = this.langiumDocuments.getOrCreateDocument(ref.sourceUri);
-            const astNode = this.astNodeLocator.getAstNode(doc, ref.sourcePath);
-            if (isParserRule(astNode) || isAction(astNode)) {
-                parserRules.add(astNode);
-            }
-        });
-        return parserRules;
-    }
 }
+
+function getParserRulesAndActionsWithReturnType(returnType: Interface | Type, references: References, langiumDocuments: LangiumDocuments, astNodeLocator: AstNodeLocator): Array<ParserRule | Action> {
+    const rules: Array<ParserRule |  Action> = [];
+    const refs = references.findReferences(returnType);
+    refs.forEach(ref => {
+        const doc = langiumDocuments.getOrCreateDocument(ref.sourceUri);
+        const astNode = astNodeLocator.getAstNode(doc, ref.sourcePath);
+        if (isParserRule(astNode) || isAction(astNode)) {
+            rules.push(astNode);
+        }
+    });
+    return rules;
+}
+
+function getLocationOfAssignment(assignment: Assignment): Location | undefined {
+    if (assignment.$cstNode) {
+        const leaf = findLeafNodeAtOffset(assignment.$cstNode, assignment.$cstNode.offset);
+        if (leaf) {
+            const rootNode = extractRootNode(leaf.element);
+            if (rootNode) {
+                return Location.create(rootNode.$document!.uri.toString(), leaf.range);
+            }
+        }
+    }
+    return undefined;
+}
+
