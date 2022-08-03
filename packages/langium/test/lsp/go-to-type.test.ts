da9f84875b753a6b6967c78501ca3ff74e38ccf8
Guillaume Fontorbe <guillaume.fontorbe@typefox.io>
2022-08-03 14:28:05 +0200

    Added tests for mergeSignatureHelpOptions

---
+++ packages/langium/test/lsp/go-to-type.test.ts
@@ -0,0 +1,87 @@
+/******************************************************************************
+ * Copyright 2021 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { SignatureHelpOptions } from 'vscode-languageserver';
+import { expectMergeSignatureHelpOptions } from '../../src/test';
+
+describe('MergeSignatureHelpOptions', () => {
+    test('Must merge SignatureHelpOptions triggerCharacters', async () => {
+        const options: SignatureHelpOptions[] = [
+            {
+                triggerCharacters: ['.']
+            },
+            {
+                triggerCharacters: ['(']
+            },
+            {
+                triggerCharacters: ['[']
+            }
+        ];
+
+        const mergedOptions: SignatureHelpOptions = {
+            triggerCharacters: ['.', '[', '(']
+        };
+        expectMergeSignatureHelpOptions({ options, mergedOptions });
+    });
+
+    test('Must merge SignatureHelpOptions retriggerCharacters', async () => {
+        const options: SignatureHelpOptions[] = [
+            {
+                triggerCharacters: ['.'],
+                retriggerCharacters: ['.']
+            },
+            {
+                triggerCharacters: ['('],
+                retriggerCharacters: ['(']
+            },
+            {
+                triggerCharacters: ['['],
+                retriggerCharacters: ['[']
+            }
+        ];
+
+        const mergedOptions: SignatureHelpOptions = {
+            triggerCharacters: ['.', '[', '('],
+            retriggerCharacters: ['.', '[', '(']
+        };
+        expectMergeSignatureHelpOptions({ options, mergedOptions });
+    });
+
+    test('Must remove duplicates from triggerCharacters', async () => {
+        const options: SignatureHelpOptions[] = [
+            {
+                triggerCharacters: ['(']
+            },
+            {
+                triggerCharacters: ['(']
+            }
+        ];
+
+        const mergedOptions: SignatureHelpOptions = {
+            triggerCharacters: ['(']
+        };
+        expectMergeSignatureHelpOptions({ options, mergedOptions });
+    });
+
+    test('Must remove duplicates from retriggerCharacters', async () => {
+        const options: SignatureHelpOptions[] = [
+            {
+                triggerCharacters: ['('],
+                retriggerCharacters: ['(']
+            },
+            {
+                triggerCharacters: ['('],
+                retriggerCharacters: ['(']
+            }
+        ];
+
+        const mergedOptions: SignatureHelpOptions = {
+            triggerCharacters: ['('],
+            retriggerCharacters: ['(']
+        };
+        expectMergeSignatureHelpOptions({ options, mergedOptions });
+    });
+});
