b8b6f0d761209ab5d1c7876c8d53b8010f84f466
goto40 <pierre.bayerl@googlemail.com>
2022-09-09 16:49:34 +0200

    prepared generator

--- examples/requirements-and-tests/src/cli/generator.ts
+++ examples/requirements-and-tests/src/cli/generator.ts
@@ -4,13 +4,26 @@ import path from 'path';
 import { RequirementModel } from '../language-server/generated/ast';
 import { extractDestinationAndName } from './cli-util';
 
-export function generateJavaScript(model: RequirementModel, filePath: string, destination: string | undefined): string {
+export function generateSummary(model: RequirementModel, filePath: string, destination: string | undefined): string {
     const data = extractDestinationAndName(filePath, destination);
-    const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
+    const generatedFilePath = `${path.join(data.destination, data.name)}.html`;
 
     const fileNode = new CompositeGeneratorNode();
-    fileNode.append('"use strict";', NL, NL);
-    model.requirements.forEach(requirement => fileNode.append(`console.log('Hello, ${requirement.name}: ${requirement.text}!');`, NL));
+    fileNode.append('<html>', NL);
+    fileNode.append('<body>', NL);
+    fileNode.append('<h1>Requirement coverage (demo generator)</h1>', NL);
+    fileNode.append('<ul>', NL);
+    fileNode.append(`<li>Source: ${model.$document?.uri.fsPath}</li>`, NL);
+    fileNode.append('</ul>', NL);
+    fileNode.append('<table border="1">', NL);
+    model.requirements.forEach(requirement => {
+        fileNode.append(`<TR><TD>${requirement.name}</TD><TD>`, NL)
+        fileNode.append(`TODO`, NL)
+        fileNode.append(`</TD></TR>`, NL)
+    });
+    fileNode.append('</table>', NL);
+    fileNode.append('</body>', NL);
+    fileNode.append('</html>', NL);
 
     if (!fs.existsSync(data.destination)) {
         fs.mkdirSync(data.destination, { recursive: true });
--- examples/requirements-and-tests/src/cli/index.ts
+++ examples/requirements-and-tests/src/cli/index.ts
@@ -2,15 +2,15 @@ import colors from 'colors';
 import { Command } from 'commander';
 import { RequirementModel } from '../language-server/generated/ast';
 import { RequirementsLanguageMetaData } from '../language-server/generated/module';
-import { createRequirementsAndTestsLanguageServices } from '../language-server/requirements-language-module';
+import { createRequirementsAndTestsLanguageServices } from '../language-server/requirements-and-tests-language-module';
 import { extractAstNode } from './cli-util';
-import { generateJavaScript } from './generator';
+import { generateSummary } from './generator';
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
     const services = createRequirementsAndTestsLanguageServices().RequirementsLanguage;
     const model = await extractAstNode<RequirementModel>(fileName, services);
-    const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
-    console.log(colors.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+    const generatedFilePath = generateSummary(model, fileName, opts.destination);
+    console.log(colors.green(`Requirement coverage generated successfully: ${generatedFilePath}`));
 };
 
 export type GenerateOptions = {
@@ -26,10 +26,10 @@ export default function(): void {
 
     const fileExtensions = RequirementsLanguageMetaData.fileExtensions.join(', ');
     program
-        .command('generate')
+        .command('generate-requirements-coverage')
         .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
         .option('-d, --destination <dir>', 'destination directory of generating')
-        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+        .description('generates a table for requirement coverage of requirements of that file')
         .action(generateAction);
 
     program.parse(process.argv);
