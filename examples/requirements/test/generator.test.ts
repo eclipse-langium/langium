a7d59555bc6eb421f8f6b8867c0c74a9bdd6f90a
goto40 <pierre.bayerl@googlemail.com>
2022-09-19 20:05:40 +0200

    fixed eslint issues

--- examples/requirements/test/generator.test.ts
+++ examples/requirements/test/generator.test.ts
@@ -1,7 +1,13 @@
-import { extractRequirementModelWithTestModels } from '../src/cli/cli-util'
-import { generateSummaryFileHTMLContent } from '../src/cli/generator'
-import { createRequirementsAndTestsLanguageServices } from '../src/language-server/requirements-and-tests-language-module'
-import * as path from 'path'
+/******************************************************************************
+ * Copyright 2022 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { extractRequirementModelWithTestModels } from '../src/cli/cli-util';
+import { generateSummaryFileHTMLContent } from '../src/cli/generator';
+import { createRequirementsAndTestsLanguageServices } from '../src/language-server/requirements-and-tests-language-module';
+import * as path from 'path';
 import { NodeFileSystem } from 'langium/node';
 
 describe('The generator should allow to extract all test cases referencing a specific requirement.', () => {
@@ -9,43 +15,43 @@ describe('The generator should allow to extract all test cases referencing a spe
         // load model
         const services = createRequirementsAndTestsLanguageServices(NodeFileSystem);
         const [requirementModel, testModels]  = await extractRequirementModelWithTestModels(
-            path.join(__dirname, "files", "good", "requirements.req"),
+            path.join(__dirname, 'files', 'good', 'requirements.req'),
             services.RequirementsLanguage
         );
-        
+
         // generate summary
         const html = generateSummaryFileHTMLContent(requirementModel, testModels);
         const table = new RegExp(/[^]*<table[^>]*>([^]*)<\/table>[^]*/gm).exec(html);
         expect(table).toBeDefined();
-        if (!table) throw new Error("unexpected");
+        if (!table) throw new Error('unexpected');
         expect(table.length).toBe(2);
-        const rows = table[1].replace(/[\n\r]/g," ").split("</TR>");
+        const rows = table[1].replace(/[\n\r]/g,' ').split('</TR>');
         const relevantRows = rows.slice(
             1, // delete last part (after last </TR>)
             rows.length-1 // delete first part (heading)
         );
-        
+
         expect(relevantRows.length).toBe(4);
         expect(relevantRows).toStrictEqual(expect.arrayContaining([
-            expect.stringMatching(new RegExp(".*ReqId000.*T005_generator.*"))
-        ]))
+            expect.stringMatching(new RegExp('.*ReqId000.*T005_generator.*'))
+        ]));
         expect(relevantRows).not.toStrictEqual(expect.arrayContaining([
-            expect.stringMatching(new RegExp(".*ReqId001_tstID.*T005_generator.*"))
-        ]))
+            expect.stringMatching(new RegExp('.*ReqId001_tstID.*T005_generator.*'))
+        ]));
 
         expect(relevantRows).toStrictEqual(expect.arrayContaining([
-            expect.stringMatching(new RegExp(".*ReqId001_tstID.*T001_good_case.*"))
-        ]))
+            expect.stringMatching(new RegExp('.*ReqId001_tstID.*T001_good_case.*'))
+        ]));
         expect(relevantRows).toStrictEqual(expect.arrayContaining([
-            expect.stringMatching(new RegExp(".*ReqId001_tstID.*T003_badTstId.*"))
-        ]))
+            expect.stringMatching(new RegExp('.*ReqId001_tstID.*T003_badTstId.*'))
+        ]));
 
         expect(relevantRows).toStrictEqual(expect.arrayContaining([
-            expect.stringMatching(new RegExp(".*ReqId002_reqID.*T001_good_case.*"))
-        ]))
+            expect.stringMatching(new RegExp('.*ReqId002_reqID.*T001_good_case.*'))
+        ]));
         expect(relevantRows).toStrictEqual(expect.arrayContaining([
-            expect.stringMatching(new RegExp(".*ReqId002_reqID.*T002_badReqId.*"))
-        ]))
+            expect.stringMatching(new RegExp('.*ReqId002_reqID.*T002_badReqId.*'))
+        ]));
 
-    })
+    });
 });
--- examples/requirements/test/validator.test.ts
+++ examples/requirements/test/validator.test.ts
@@ -1,13 +1,19 @@
-import { extractDocuments } from '../src/cli/cli-util'
-import { createRequirementsAndTestsLanguageServices } from '../src/language-server/requirements-and-tests-language-module'
-import * as path from 'path'
+/******************************************************************************
+ * Copyright 2022 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { extractDocuments } from '../src/cli/cli-util';
+import { createRequirementsAndTestsLanguageServices } from '../src/language-server/requirements-and-tests-language-module';
+import * as path from 'path';
 import { NodeFileSystem } from 'langium/node';
 
 describe('A requirement identifier and a test identifier shall contain a number.', () => {
     test('T001_good_case', async () => {
         const services = createRequirementsAndTestsLanguageServices(NodeFileSystem);
         const [mainDoc,allDocs] = await extractDocuments(
-            path.join(__dirname, "files", "good", "requirements.req"),
+            path.join(__dirname, 'files', 'good', 'requirements.req'),
             services.RequirementsLanguage
         );
         expect((mainDoc.diagnostics ?? [])).toEqual([]);
@@ -22,7 +28,7 @@ describe('A requirement identifier shall contain a number.', () => {
     test('T002_badReqId: bad case', async () => {
         const services = createRequirementsAndTestsLanguageServices(NodeFileSystem);
         const [mainDoc,] = await extractDocuments(
-            path.join(__dirname, "files", "bad1", "requirements.req"),
+            path.join(__dirname, 'files', 'bad1', 'requirements.req'),
             services.RequirementsLanguage
         );
         expect(mainDoc.diagnostics ?? []).toEqual(expect.arrayContaining([
@@ -39,12 +45,12 @@ describe('A test identifier shall contain a number.', () => {
     test('T003_badTstId: bad case', async () => {
         const services = createRequirementsAndTestsLanguageServices(NodeFileSystem);
         const [,allDocs] = await extractDocuments(
-            path.join(__dirname, "files", "bad1", "requirements.req"),
+            path.join(__dirname, 'files', 'bad1', 'requirements.req'),
             services.RequirementsLanguage
         );
         const doc = allDocs.find(doc=>/tests_part1.tst/.test(doc.uri.fsPath));
         expect(doc).toBeDefined();
-        if (!doc) throw new Error("impossible");        
+        if (!doc) throw new Error('impossible');
         expect(doc.diagnostics ?? []).toEqual(expect.arrayContaining([
             expect.objectContaining({
                 message: expect.stringMatching('Test name TA should container a number.'),
@@ -58,7 +64,7 @@ describe('A requirement shall be covered by at least one test.', () => {
     test('T004_cov: bad case', async () => {
         const services = createRequirementsAndTestsLanguageServices(NodeFileSystem);
         const [mainDoc,] = await extractDocuments(
-            path.join(__dirname, "files", "bad1", "requirements.req"),
+            path.join(__dirname, 'files', 'bad1', 'requirements.req'),
             services.RequirementsLanguage
         );
         expect(mainDoc.diagnostics ?? []).toEqual(expect.arrayContaining([
@@ -74,12 +80,12 @@ describe('A referenced environment in a test must be found in one of the referen
     test('referenced environment test', async () => {
         const services = createRequirementsAndTestsLanguageServices(NodeFileSystem);
         const [,allDocs] = await extractDocuments(
-            path.join(__dirname, "files", "bad2", "requirements.req"),
+            path.join(__dirname, 'files', 'bad2', 'requirements.req'),
             services.RequirementsLanguage
         );
         const doc = allDocs.find(doc=>/tests_part1.tst/.test(doc.uri.fsPath));
         expect(doc).toBeDefined();
-        if (!doc) throw new Error("impossible");
+        if (!doc) throw new Error('impossible');
         expect((doc.diagnostics ?? [])).toEqual(expect.arrayContaining([
             expect.objectContaining({
                 message: expect.stringMatching('Test T002_badReqId references environment Linux_x86 which is used in any referenced requirement.'),
