c37fd4b8a94c7858f9ca384e7012c7d4280360a6
emilkrebs <onlime@gmx.de>
2022-11-26 22:45:54 +0100

    Added mainbrowser to the arithmetics example

---
+++ examples/arithmetics/src/language-server/main-browser.ts
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * Copyright 2022 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { startLanguageServer, EmptyFileSystem } from 'langium';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser';
+import { createArithmeticsServices } from './arithmetics-module';
+
+/* browser specific setup code */
+const messageReader = new BrowserMessageReader(self);
+const messageWriter = new BrowserMessageWriter(self);
+
+const connection = createConnection(messageReader, messageWriter);
+
+// Inject the shared services and language-specific services
+const { shared } = createArithmeticsServices({ connection, ...EmptyFileSystem });
+
+// Start the language server with the shared services
+startLanguageServer(shared);
---
+++ examples/arithmetics/syntaxes/arithmetics.monarch.ts
@@ -0,0 +1,29 @@
+// Monarch syntax highlighting for the arithmetics language.
+export default {
+    keywords: [
+        'def','module'
+    ],
+    operators: [
+        '-',',',';',':','*','/','+'
+    ],
+    symbols:  /-|,|;|:|\(|\)|\*|/|\+/,
+
+    tokenizer: {
+        initial: [
+            { regex: /[_a-zA-Z][\w_]*/, action: { cases: { '@keywords': {"token":"keyword"}, '@default': {"token":"ID"} }} },
+            { regex: /[0-9]+(\.[0-9]*)?/, action: {"token":"number"} },
+            { include: '@whitespace' },
+            { regex: /@symbols/, action: { cases: { '@operators': {"token":"operator"}, '@default': {"token":""} }} },
+        ],
+        whitespace: [
+            { regex: /\s+/, action: {"token":"white"} },
+            { regex: /\/\*/, action: {"token":"comment","next":"@comment"} },
+            { regex: /\/\/[^\n\r]*/, action: {"token":"comment"} },
+        ],
+        comment: [
+            { regex: /[^\/\*]+/, action: {"token":"comment"} },
+            { regex: /\*\//, action: {"token":"comment","next":"@pop"} },
+            { regex: /[\/\*]/, action: {"token":"comment"} },
+        ],
+    }
+};
