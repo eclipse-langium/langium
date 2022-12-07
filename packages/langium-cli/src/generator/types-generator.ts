7f5b7ad8cb2cf6c42197e48002c9d23b234ac5bf
Dennis Huebner <dennis.huebner@gmail.com>
2022-12-07 12:22:47 +0100

    langium-cli to generate types definition (#754)

---
+++ packages/langium-cli/src/generator/types-generator.ts
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Copyright 2022 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { CompositeGeneratorNode, Grammar, LangiumServices, NL, processGeneratorNode } from 'langium';
+import { collectAst, distinctAndSorted, PropertyType } from 'langium/lib/grammar/type-system';
+import { LangiumGrammarGrammar } from 'langium/lib/grammar/generated/grammar';
+import { collectKeywords } from './util';
+
+export function generateTypesFile(services: LangiumServices, grammars: Grammar[]): string {
+    const astTypes = collectAst(services.shared.workspace.LangiumDocuments, grammars);
+    const fileNode = new CompositeGeneratorNode();
+    const reservedWords = new Set(collectKeywords(LangiumGrammarGrammar()));
+    astTypes.unions.filter((union) => !astTypes.interfaces.find((iface) =>
+        iface.name === union.name
+    )).forEach((union) => {
+        fileNode.append(`type ${escapeReservedWords(union.name, reservedWords)} = ${propertyTypesToString(union.union)};`);
+        fileNode.append(NL).append(NL);
+    });
+    astTypes.interfaces.forEach((iFace) => {
+        fileNode.append(`interface ${escapeReservedWords(iFace.name, reservedWords)}${iFace.interfaceSuperTypes.length > 0 ? (' extends ' + Array.from(iFace.interfaceSuperTypes).join(', ')) : ''} {`);
+        fileNode.append(NL);
+        fileNode.indent((body) => {
+            iFace.properties.forEach(property =>
+                body.append(`${escapeReservedWords(property.name, reservedWords)}${property.optional ? '?' : ''}: ${propertyTypesToString(property.typeAlternatives)}`).append(NL)
+            );
+        });
+        fileNode.append('}').append(NL).append(NL);
+    }
+    );
+    return processGeneratorNode(fileNode);
+}
+
+function propertyTypesToString(alternatives: PropertyType[]): string {
+    return distinctAndSorted(alternatives.map(typePropertyToString)).join(' | ');
+}
+
+function typePropertyToString(propertyType: PropertyType): string {
+    let res = distinctAndSorted(propertyType.types).join(' | ');
+    res = propertyType.reference ? `@${res}` : res;
+    res = propertyType.array ? `${res}[]` : res;
+    return res;
+}
+
+function escapeReservedWords(name: string, reserved: Set<string>): string {
+    return reserved.has(name) ? `^${name}` : name;
+}
---
+++ packages/langium-cli/test/generator/types-generator.test.ts
@@ -0,0 +1,109 @@
+/******************************************************************************
+ * Copyright 2022 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { createLangiumGrammarServices, EmptyFileSystem, Grammar } from 'langium';
+import { parseHelper } from 'langium/test';
+import { generateTypesFile } from '../../src/generator/types-generator';
+
+const { grammar } = createLangiumGrammarServices(EmptyFileSystem);
+
+describe('Types generator', () => {
+
+    test('should generate types file', async () => {
+        const result = (await parseHelper<Grammar>(grammar)(TEST_GRAMMAR)).parseResult;
+        const typesFileContent = generateTypesFile(grammar, [result.value]);
+        expect(typesFileContent).toMatch(EXPECTED_TYPES);
+    });
+
+});
+
+const EXPECTED_TYPES =
+    `type AbstractDefinition = DeclaredParameter | Definition;
+
+type Expression = BinaryExpression | FunctionCall | NumberLiteral;
+
+type Statement = Definition | Evaluation;
+
+interface BinaryExpression {
+    left: Expression
+    operator: '*' | '+' | '-' | '/'
+    right: Expression
+}
+
+interface DeclaredParameter {
+    name: string
+}
+
+interface Definition {
+    name: string
+    args?: DeclaredParameter[]
+    expr: Expression
+}
+
+interface Evaluation {
+    expression: Expression
+}
+
+interface FunctionCall {
+    func: @AbstractDefinition
+    args?: Expression[]
+}
+
+interface Module {
+    name: string
+    statements?: Statement[]
+}
+
+interface NumberLiteral {
+    value: number
+}
+
+`;
+
+const TEST_GRAMMAR =
+    `
+grammar Arithmetics
+
+entry Module:
+    'module' name=ID
+    (statements+=Statement)*;
+
+Statement:
+    Definition | Evaluation;
+
+Definition:
+    'def' name=ID ('(' args+=DeclaredParameter (',' args+=DeclaredParameter)* ')')?
+    ':' expr=Expression ';';
+
+DeclaredParameter:
+    name=ID;
+
+type AbstractDefinition = Definition | DeclaredParameter;
+
+Evaluation:
+    expression=Expression ';';
+
+Expression:
+    Addition;
+
+Addition infers Expression:
+    Multiplication ({infer BinaryExpression.left=current} operator=('+' | '-') right=Multiplication)*;
+
+Multiplication infers Expression:
+    PrimaryExpression ({infer BinaryExpression.left=current} operator=('*' | '/') right=PrimaryExpression)*;
+
+PrimaryExpression infers Expression:
+    '(' Expression ')' |
+    {infer NumberLiteral} value=NUMBER |
+    {infer FunctionCall} func=[AbstractDefinition] ('(' args+=Expression (',' args+=Expression)* ')')?;
+
+hidden terminal WS: /\\s+/;
+terminal ID: /[_a-zA-Z][\\w_]*/;
+terminal NUMBER returns number: /[0-9]+(\\.[0-9]*)?/;
+
+hidden terminal ML_COMMENT: /\\/\\*[\\s\\S]*?\\*\\//;
+hidden terminal SL_COMMENT: /\\/\\/[^\\n\\r]*/;
+`;
