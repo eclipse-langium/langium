7383078c639de0e6ac56f63a19d9e9382b9e0bee
Miro Spönemann <miro.spoenemann@typefox.io>
2022-12-21 13:08:59 +0100

    Added notifications with AST contents on document change (#848)

--- examples/arithmetics/src/language-server/main-browser.ts
+++ examples/arithmetics/src/language-server/main-browser.ts
@@ -4,8 +4,8 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
-import { startLanguageServer, EmptyFileSystem } from 'langium';
-import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser';
+import { startLanguageServer, EmptyFileSystem, DocumentState } from 'langium';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection, Diagnostic, NotificationType } from 'vscode-languageserver/browser';
 import { createArithmeticsServices } from './arithmetics-module';
 
 /* browser specific setup code */
@@ -15,7 +15,22 @@ const messageWriter = new BrowserMessageWriter(self);
 const connection = createConnection(messageReader, messageWriter);
 
 // Inject the shared services and language-specific services
-const { shared } = createArithmeticsServices({ connection, ...EmptyFileSystem });
+const { shared, arithmetics } = createArithmeticsServices({ connection, ...EmptyFileSystem });
 
 // Start the language server with the shared services
 startLanguageServer(shared);
+
+// Send a notification with the serialized AST after every document change
+type DocumentChange = { uri: string, content: string, diagnostics: Diagnostic[] };
+const documentChangeNotification = new NotificationType<DocumentChange>('browser/DocumentChange');
+const jsonSerializer = arithmetics.serializer.JsonSerializer;
+shared.workspace.DocumentBuilder.onBuildPhase(DocumentState.Validated, documents => {
+    for (const document of documents) {
+        const json = jsonSerializer.serialize(document.parseResult.value);
+        connection.sendNotification(documentChangeNotification, {
+            uri: document.uri.toString(),
+            content: json,
+            diagnostics: document.diagnostics ?? []
+        });
+    }
+});
--- examples/domainmodel/src/language-server/main-browser.ts
+++ examples/domainmodel/src/language-server/main-browser.ts
@@ -4,8 +4,8 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
-import { startLanguageServer, EmptyFileSystem } from 'langium';
-import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser';
+import { startLanguageServer, EmptyFileSystem, DocumentState } from 'langium';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection, Diagnostic, NotificationType } from 'vscode-languageserver/browser';
 import { createDomainModelServices } from './domain-model-module';
 
 /* browser specific setup code */
@@ -15,7 +15,22 @@ const messageWriter = new BrowserMessageWriter(self);
 const connection = createConnection(messageReader, messageWriter);
 
 // Inject the shared services and language-specific services
-const { shared } = createDomainModelServices({ connection, ...EmptyFileSystem });
+const { shared, domainmodel } = createDomainModelServices({ connection, ...EmptyFileSystem });
 
 // Start the language server with the shared services
 startLanguageServer(shared);
+
+// Send a notification with the serialized AST after every document change
+type DocumentChange = { uri: string, content: string, diagnostics: Diagnostic[] };
+const documentChangeNotification = new NotificationType<DocumentChange>('browser/DocumentChange');
+const jsonSerializer = domainmodel.serializer.JsonSerializer;
+shared.workspace.DocumentBuilder.onBuildPhase(DocumentState.Validated, documents => {
+    for (const document of documents) {
+        const json = jsonSerializer.serialize(document.parseResult.value);
+        connection.sendNotification(documentChangeNotification, {
+            uri: document.uri.toString(),
+            content: json,
+            diagnostics: document.diagnostics ?? []
+        });
+    }
+});
