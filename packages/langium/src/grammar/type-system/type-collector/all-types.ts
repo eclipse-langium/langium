929e261de07d4591e9faa4fd489875a991151738
Daumantas Kavolis <daumantas.kavolis@sensmetry.com>
2023-02-06 17:58:03 +0200

    Fixed undefined access error and stackoverflow  (#897)

--- packages/langium/src/grammar/type-system/type-collector/all-types.ts
+++ packages/langium/src/grammar/type-system/type-collector/all-types.ts
@@ -56,8 +56,9 @@ function addSuperProperties(allTypes: InterfaceType[]) {
         }
     }
 
+    const visited = new Set<InterfaceType>();
     for (const type of allTypes) {
-        addSuperPropertiesInternal(type);
+        addSuperPropertiesInternal(type, visited);
     }
 }
 
