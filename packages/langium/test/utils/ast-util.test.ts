10a963b2dba58adeae7e6f12365dcf0384860b96
Mark Sujew <mark.sujew@typefox.io>
2023-02-08 14:10:22 +0000

    Preemptive explicit vitest import

--- packages/langium/test/utils/ast-util.test.ts
+++ packages/langium/test/utils/ast-util.test.ts
@@ -4,6 +4,7 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
+import { describe, expect, test } from 'vitest';
 import { createLangiumGrammarServices, EmptyFileSystem, streamAst } from '../../src';
 import { isParserRule } from '../../src/grammar/generated/ast';
 
