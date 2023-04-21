18924e06b0c71948808e8bd57836b406721dc45a
Mark Sujew <mark.sujew@typefox.io>
2023-04-21 11:15:50 +0000

    Fix script

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -6,6 +6,7 @@ async function runUpdate() {
     const langiumPackage = await fs.readJson(langiumPath);
     const version = langiumPackage.version;
     await Promise.all([
+        replaceAll('langium', true, version),
         replaceAll('langium-cli', true, version),
         replaceAll('langium-sprotty', true, version),
         replaceAll('langium-vscode', true, version),
@@ -21,8 +22,8 @@ async function replaceAll(project, package, version, dot) {
     const path = getPath(project, package, dot);
     let content = await fs.readFile(path, 'utf-8');
     content = content
-        .replace(/(?<="langium": "~)\d+\.\d+\.\d+/g, version)
-        .replace(/(?<="langium-cli": "~)\d+\.\d+\.\d+/g, version);
+        .replace(/(?<="langium": "[~\^]?)\d+\.\d+\.\d+/g, version)
+        .replace(/(?<="langium-cli": "[~\^]?)\d+\.\d+\.\d+/g, version);
     await fs.writeFile(path, content);
 }
 
