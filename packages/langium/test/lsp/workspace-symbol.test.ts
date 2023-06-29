9d423b1ead922a5d567f1c9f9a011557312b68eb
Mark Sujew <mark.sujew@typefox.io>
2023-06-29 15:14:47 +0200

    Add tests for workspace symbols

---
+++ packages/langium/test/lsp/workspace-symbol.test.ts
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * Copyright 2021 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { describe, test } from 'vitest';
+import { createServicesForGrammar } from '../../src';
+import { expectWorkspaceSymbols, parseHelper } from '../../src/test';
+
+const grammar = `
+ grammar HelloWorld
+ entry Model: persons+=Person;
+ Person: 'Person' name=ID;
+ terminal ID: /\\w+/;
+ hidden terminal WS: /\\s+/;
+ `.trim();
+
+describe('Workspace symbols', () => {
+
+    test('Should show all workspace symbols', async () => {
+        const helloWorldServices = await createServicesForGrammar({
+            grammar
+        });
+        const symbols = expectWorkspaceSymbols(helloWorldServices.shared);
+        const parser = parseHelper(helloWorldServices);
+        await parser('Person Alice');
+        await parser('Person Bob');
+        await symbols({
+            expectedSymbols: [
+                'Alice',
+                'Bob'
+            ]
+        });
+    });
+
+    test('Should show all workspace symbols matching the query', async () => {
+        const helloWorldServices = await createServicesForGrammar({
+            grammar
+        });
+        const symbols = expectWorkspaceSymbols(helloWorldServices.shared);
+        const parser = parseHelper(helloWorldServices);
+        await parser('Person Alice');
+        await parser('Person Bob');
+        await symbols({
+            query: 'Ali',
+            expectedSymbols: [
+                'Alice'
+            ]
+        });
+    });
+});
+
