d76bb6af9e217cc2496167e4ea6a05500cfb02fa
Mark Sujew <mark.sujew@typefox.io>
2023-06-29 14:54:04 +0200

    Fix child lookup for workspace symbols

--- packages/langium/src/lsp/workspace-symbol-provider.ts
+++ packages/langium/src/lsp/workspace-symbol-provider.ts
@@ -4,7 +4,7 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
-import type { WorkspaceSymbol, WorkspaceSymbolParams } from 'vscode-languageserver';
+import type { DocumentSymbol, WorkspaceSymbol, WorkspaceSymbolParams } from 'vscode-languageserver';
 import type { LangiumSharedServices } from '../services';
 import type { ServiceRegistry } from '../service-registry';
 import type { LangiumDocuments } from '../workspace';
@@ -28,6 +28,7 @@ export class DefaultWorkspaceSymbolProvider implements WorkspaceSymbolProvider {
 
     async getSymbols(params: WorkspaceSymbolParams, cancelToken = CancellationToken.None): Promise<WorkspaceSymbol[]> {
         const workspaceSymbols: WorkspaceSymbol[] = [];
+        const query = params.query.toLowerCase();
         for (const document of this.documents.all) {
             await interruptAndCheck(cancelToken);
             const languageServices = this.serviceRegistry.getServices(document.uri);
@@ -38,19 +39,32 @@ export class DefaultWorkspaceSymbolProvider implements WorkspaceSymbolProvider {
                     textDocument: TextDocumentIdentifier.create(uri)
                 }, cancelToken);
                 for (const symbol of symbols) {
-                    if (!params.query || symbol.name.toLowerCase().startsWith(params.query)) {
-                        workspaceSymbols.push({
-                            kind: symbol.kind,
-                            name: symbol.name,
-                            location: {
-                                range: symbol.range,
-                                uri
-                            }
-                        });
-                    }
+                    this.processDocumentSymbol(symbol, uri, query, workspaceSymbols);
                 }
             }
         }
         return workspaceSymbols;
+    }
+
+    protected processDocumentSymbol(symbol: DocumentSymbol, uri: string, query: string, list: WorkspaceSymbol[]): void {
+        if (!query || symbol.name.toLowerCase().startsWith(query)) {
+            list.push({
+                kind: symbol.kind,
+                name: symbol.name,
+                location: {
+                    range: symbol.range,
+                    uri
+                }
+            });
+        }
+        if (symbol.children && this.shouldProcessDocumentSymbolChildren(symbol)) {
+            for (const child of symbol.children) {
+                this.processDocumentSymbol(child, uri, query, list);
+            }
+        }
+    }
+
+    protected shouldProcessDocumentSymbolChildren(_symbol: DocumentSymbol): boolean {
+        return true;
     }
 }
