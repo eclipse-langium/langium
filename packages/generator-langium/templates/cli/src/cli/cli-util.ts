e1491601b0f0592494cf4e8e3a42b8ea6cee31aa
Mark Sujew <mark.sujew@typefox.io>
2023-08-17 11:58:48 +0200

    Fix CLI yeoman generator

--- packages/generator-langium/templates/cli/src/cli/cli-util.ts
+++ packages/generator-langium/templates/cli/src/cli/cli-util.ts
@@ -17,7 +17,7 @@ export async function extractDocument(fileName: string, services: LangiumService
     }
 
     const document = services.shared.workspace.LangiumDocuments.getOrCreateDocument(URI.file(path.resolve(fileName)));
-    await services.shared.workspace.DocumentBuilder.build([document], { validationChecks: 'all' });
+    await services.shared.workspace.DocumentBuilder.build([document], { validation: true });
 
     const validationErrors = (document.diagnostics ?? []).filter(e => e.severity === 1);
     if (validationErrors.length > 0) {
