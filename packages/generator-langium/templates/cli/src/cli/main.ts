03e0cbf7f5fc19822c21a496466964cd02ced7c9
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2023-09-27 15:30:02 +0200

    Fix generator-langium cli for esm (#1201)
    Signed-off-by: Christian Dietrich <christian.dietrich.opensource@gmail.com>

--- packages/generator-langium/templates/cli/src/cli/main.ts
+++ packages/generator-langium/templates/cli/src/cli/main.ts
@@ -6,6 +6,13 @@ import { create<%= LanguageName %>Services } from '../language/<%= language-id %
 import { extractAstNode } from './cli-util.js';
 import { generateJavaScript } from './generator.js';
 import { NodeFileSystem } from 'langium/node';
+import * as url from 'node:url';
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const packagePath = path.resolve(__dirname, '..', '..', 'package.json');
+const packageContent = await fs.readFile(packagePath, 'utf-8');
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
     const services = create<%= LanguageName %>Services(NodeFileSystem).<%= LanguageName %>;
@@ -21,9 +28,7 @@ export type GenerateOptions = {
 export default function(): void {
     const program = new Command();
 
-    program
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        .version(require('../../package.json').version);
+    program.version(JSON.parse(packageContent).version);
 
     const fileExtensions = <%= LanguageName %>LanguageMetaData.fileExtensions.join(', ');
     program
