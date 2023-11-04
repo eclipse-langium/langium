8225603acdbec62776c0369c7f99d605de2a0626
Reda Al Sulais <u.yokozuna@gmail.com>
2023-11-04 20:51:58 +0300

    feat(example): validate commands and unused symbols in `statemachine`

--- examples/statemachine/src/language-server/statemachine-validator.ts
+++ examples/statemachine/src/language-server/statemachine-validator.ts
@@ -5,16 +5,25 @@
  ******************************************************************************/
 
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { State, Statemachine, StatemachineAstType, Event } from './generated/ast.js';
+import type { State, Statemachine, StatemachineAstType, Event, Command } from './generated/ast.js';
 import type { StatemachineServices } from './statemachine-module.js';
-import { MultiMap } from 'langium';
+import { MultiMap, diagnosticData } from 'langium';
+
+export namespace IssueCodes {
+    export const StateNameUppercase = 'state-name-uppercase';
+    export const UnusedSymbol = 'unused-symbol';
+}
 
 export function registerValidationChecks(services: StatemachineServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.StatemachineValidator;
     const checks: ValidationChecks<StatemachineAstType> = {
         State: validator.checkStateNameStartsWithCapital,
-        Statemachine: validator.checkUniqueStatesAndEvents
+        Statemachine: [
+            validator.checkUniqueSymbolName,
+            validator.checkUnreachedStates,
+            validator.checkUnusedCommandsAndEvents,
+        ]
     };
     registry.register(checks, validator);
 }
@@ -29,20 +38,24 @@ export class StatemachineValidator {
         if (state.name) {
             const firstChar = state.name.substring(0, 1);
             if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'State name should start with a capital letter.', { node: state, property: 'name' });
+                accept('warning', 'State name should start with a capital letter.', {
+                    node: state,
+                    property: 'name',
+                    data: diagnosticData(IssueCodes.StateNameUppercase),
+                });
             }
         }
     }
 
     /**
-     * Checks if there are duplicate state and event names.
+     * Checks if there are duplicate command, event, and state names.
      * @param statemachine the statemachine to check
      * @param accept the acceptor to report errors
      */
-    checkUniqueStatesAndEvents(statemachine: Statemachine, accept: ValidationAcceptor): void {
+    checkUniqueSymbolName(statemachine: Statemachine, accept: ValidationAcceptor): void {
         // check for duplicate state and event names and add them to the map
-        const names = new MultiMap<string, State | Event>();
-        const allSymbols = [...statemachine.states, ...statemachine.events];
+        const names = new MultiMap<string, Command | Event | State>();
+        const allSymbols = [...statemachine.commands, ...statemachine.events, ...statemachine.states];
         for (const symbol of allSymbols) {
             names.add(symbol.name, symbol);
         }
@@ -52,6 +65,74 @@ export class StatemachineValidator {
                     accept('error', `Duplicate identifier name: ${name}`, { node: symbol, property: 'name' });
                 }
             }
+        }
+    }
+
+    /**
+     * Checks if there are unreached state.
+     * @param statemachine the statemachine to check
+     * @param accept the acceptor to report errors
+     */
+    checkUnreachedStates(statemachine: Statemachine, accept: ValidationAcceptor): void {
+        const states = new Map<string, State>();
+        for (const state of statemachine.states) {
+            states.set(state.name, state);
+        }
+
+        for (const { transitions } of statemachine.states) {
+            for (const { state: { ref: stateRef } } of transitions) {
+                if (stateRef && states.has(stateRef.name)) {
+                    states.delete(stateRef.name);
+                }
+            }
+        }
+
+        for (const [name, state] of states.entries()) {
+            accept('warning', `Unreached state name: ${name}`, {
+                node: state,
+                property: 'name',
+                data: diagnosticData(IssueCodes.UnusedSymbol),
+                tags: [1],
+            });
+        }
+    }
+
+    /**
+     * Checks if there are unused commands and events.
+     * @param statemachine the statemachine to check
+     * @param accept the acceptor to report errors
+     */
+    checkUnusedCommandsAndEvents(statemachine: Statemachine, accept: ValidationAcceptor): void {
+        const commands = new Map<string, Command>();
+        for (const command of statemachine.commands) {
+            commands.set(command.name, command);
+        }
+
+        const events = new Map<string, Event>();
+        for (const event of statemachine.events) {
+            events.set(event.name, event);
+        }
+
+        for (const { actions, transitions } of statemachine.states) {
+            for (const { ref } of actions) {
+                if (ref && commands.has(ref.name)) {
+                    commands.delete(ref.name);
+                }
+            }
+            for (const { event: { ref: refEvent } } of transitions) {
+                if (refEvent && events.has(refEvent.name)) {
+                    events.delete(refEvent.name);
+                }
+            }
+        }
+
+        for (const [name, symbol] of [...commands.entries(), ...events.entries()]) {
+            accept('warning', `Unused command or event name: ${name}`, {
+                node: symbol,
+                property: 'name',
+                data: diagnosticData(IssueCodes.UnusedSymbol),
+                tags: [1],
+            });
         }
     }
 }
