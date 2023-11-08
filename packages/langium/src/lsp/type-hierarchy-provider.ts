04e868ae3051dca4c76d0aec6aba661c48b0a936
Lars Reimann <mail@larsreimann.com>
2023-11-08 10:04:27 +0100

    `TypeHierarchyProvider` and `AbstractTypeHierarchyProvider`

---
+++ packages/langium/src/lsp/type-hierarchy-provider.ts
@@ -0,0 +1,134 @@
+/******************************************************************************
+ * Copyright 2023 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import type {
+    CancellationToken,
+    TypeHierarchyItem,
+    TypeHierarchyPrepareParams,
+    TypeHierarchySubtypesParams,
+    TypeHierarchySupertypesParams
+} from 'vscode-languageserver';
+import { SymbolKind } from 'vscode-languageserver';
+import type { GrammarConfig } from '../grammar/grammar-config.js';
+import type { NameProvider } from '../references/name-provider.js';
+import type { References } from '../references/references.js';
+import type { LangiumServices } from '../services.js';
+import type { AstNode } from '../syntax-tree.js';
+import { findDeclarationNodeAtOffset } from '../utils/cst-util.js';
+import type { Stream } from '../utils/stream.js';
+import { URI } from '../utils/uri-util.js';
+import type { ReferenceDescription } from '../workspace/ast-descriptions.js';
+import type { LangiumDocument, LangiumDocuments } from '../workspace/documents.js';
+
+/**
+ * Language-specific service for handling type hierarchy requests.
+ */
+
+export interface TypeHierarchyProvider {
+    prepareTypeHierarchy(document: LangiumDocument, params: TypeHierarchyPrepareParams, cancelToken?: CancellationToken): TypeHierarchyItem[] | undefined;
+
+    supertypes(params: TypeHierarchySupertypesParams, cancelToken?: CancellationToken): TypeHierarchyItem[] | undefined;
+
+    subtypes(params: TypeHierarchySubtypesParams, cancelToken?: CancellationToken): TypeHierarchyItem[] | undefined;
+}
+
+export abstract class AbstractTypeHierarchyProvider implements TypeHierarchyProvider {
+    protected readonly grammarConfig: GrammarConfig;
+    protected readonly nameProvider: NameProvider;
+    protected readonly documents: LangiumDocuments;
+    protected readonly references: References;
+
+    constructor(services: LangiumServices) {
+        this.grammarConfig = services.parser.GrammarConfig;
+        this.nameProvider = services.references.NameProvider;
+        this.documents = services.shared.workspace.LangiumDocuments;
+        this.references = services.references.References;
+    }
+
+    prepareTypeHierarchy(document: LangiumDocument, params: TypeHierarchyPrepareParams, _cancelToken?: CancellationToken): TypeHierarchyItem[] | undefined {
+        const rootNode = document.parseResult.value;
+        const targetNode = findDeclarationNodeAtOffset(
+            rootNode.$cstNode,
+            document.textDocument.offsetAt(params.position),
+            this.grammarConfig.nameRegexp,
+        );
+        if (!targetNode) {
+            return undefined;
+        }
+
+        const declarationNode = this.references.findDeclarationNode(targetNode);
+        if (!declarationNode) {
+            return undefined;
+        }
+
+        return this.getTypeHierarchyItems(declarationNode.astNode, document);
+    }
+
+    protected getTypeHierarchyItems(targetNode: AstNode, document: LangiumDocument): TypeHierarchyItem[] | undefined {
+        const nameNode = this.nameProvider.getNameNode(targetNode);
+        const name = this.nameProvider.getName(targetNode);
+        if (!nameNode || !targetNode.$cstNode || name === undefined) {
+            return undefined;
+        }
+
+        return [
+            {
+                kind: SymbolKind.Class,
+                name,
+                range: targetNode.$cstNode.range,
+                selectionRange: nameNode.range,
+                uri: document.uri.toString(),
+                ...this.getTypeHierarchyItem(targetNode),
+            },
+        ];
+    }
+
+    protected getTypeHierarchyItem(_targetNode: AstNode): Partial<TypeHierarchyItem> | undefined {
+        return undefined;
+    }
+
+    supertypes(params: TypeHierarchySupertypesParams, _cancelToken?: CancellationToken): TypeHierarchyItem[] | undefined {
+        const document = this.documents.getOrCreateDocument(URI.parse(params.item.uri));
+        const rootNode = document.parseResult.value;
+        const targetNode = findDeclarationNodeAtOffset(
+            rootNode.$cstNode,
+            document.textDocument.offsetAt(params.item.range.start),
+            this.grammarConfig.nameRegexp,
+        );
+        if (!targetNode) {
+            return undefined;
+        }
+        return this.getSupertypes(targetNode.astNode);
+    }
+
+    /**
+     * Override this method to collect the supertypes for your language.
+     */
+    protected abstract getSupertypes(node: AstNode): TypeHierarchyItem[] | undefined;
+
+    subtypes(params: TypeHierarchySubtypesParams, _cancelToken?: CancellationToken): TypeHierarchyItem[] | undefined {
+        const document = this.documents.getOrCreateDocument(URI.parse(params.item.uri));
+        const rootNode = document.parseResult.value;
+        const targetNode = findDeclarationNodeAtOffset(
+            rootNode.$cstNode,
+            document.textDocument.offsetAt(params.item.range.start),
+            this.grammarConfig.nameRegexp,
+        );
+        if (!targetNode) {
+            return undefined;
+        }
+
+        const references = this.references.findReferences(targetNode.astNode, {
+            includeDeclaration: false,
+        });
+        return this.getSubtypes(targetNode.astNode, references);
+    }
+
+    /**
+     * Override this method to collect the subtypes for your language.
+     */
+    protected abstract getSubtypes(node: AstNode, references: Stream<ReferenceDescription>): TypeHierarchyItem[] | undefined;
+}
