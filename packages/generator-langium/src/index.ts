02b3c92388bf6740cf1dc879f22c2b4ae5cd73f6
Emil Krebs <68400102+emilkrebs@users.noreply.github.com>
2023-11-16 14:00:15 +0100

    Updated Yeoman (#1268)

--- packages/generator-langium/src/index.ts
+++ packages/generator-langium/src/index.ts
@@ -56,7 +56,7 @@ function description(...d: string[]): string {
 class LangiumGenerator extends Generator {
     private answers: Answers;
 
-    constructor(args: string | string[], options: Generator.GeneratorOptions) {
+    constructor(args: string | string[], options: Record<string, unknown>) {
         super(args, options);
     }
 
@@ -110,8 +110,7 @@ class LangiumGenerator extends Generator {
                     'Your language can be run inside of a VSCode extension.'
                 ),
                 message: 'Include VSCode extension?',
-                default: 'yes',
-                choices: ['yes', 'no']
+                default: 'yes'
             },
             {
                 type: 'confirm',
@@ -287,7 +286,7 @@ class LangiumGenerator extends Generator {
         return this.destinationPath(USER_DIR, this.answers.extensionName, ...path);
     }
 
-    _replaceTemplateWords(fileExtensionGlob: string, languageName: string, languageId: string, content: Buffer): string {
+    _replaceTemplateWords(fileExtensionGlob: string, languageName: string, languageId: string, content: string | Buffer): string {
         return content.toString()
             .replace(EXTENSION_NAME, this.answers.extensionName)
             .replace(RAW_LANGUAGE_NAME, this.answers.rawLanguageName)
