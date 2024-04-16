af9fc1cbf411b13600b1e2af4a38907b23301ec1
Mark Sujew <mark.sujew@typefox.io>
2024-04-16 15:41:48 +0200

    Filter out document changes to unknown file types

--- packages/langium/src/lsp/document-update-handler.ts
+++ packages/langium/src/lsp/document-update-handler.ts
@@ -12,6 +12,7 @@ import type { TextDocument } from '../workspace/documents.js';
 import type { WorkspaceLock } from '../workspace/workspace-lock.js';
 import type { LangiumSharedServices } from './lsp-services.js';
 import type { WorkspaceManager } from '../workspace/workspace-manager.js';
+import type { ServiceRegistry } from '../service-registry.js';
 
 /**
  * Shared service for handling text document changes and watching relevant files.
@@ -35,11 +36,13 @@ export class DefaultDocumentUpdateHandler implements DocumentUpdateHandler {
     protected readonly workspaceManager: WorkspaceManager;
     protected readonly documentBuilder: DocumentBuilder;
     protected readonly workspaceLock: WorkspaceLock;
+    protected readonly serviceRegistry: ServiceRegistry;
 
     constructor(services: LangiumSharedServices) {
         this.workspaceManager = services.workspace.WorkspaceManager;
         this.documentBuilder = services.workspace.DocumentBuilder;
         this.workspaceLock = services.workspace.WorkspaceLock;
+        this.serviceRegistry = services.ServiceRegistry;
 
         let canRegisterFileWatcher = false;
         services.lsp.LanguageServer.onInitialize(params => {
@@ -73,6 +76,9 @@ export class DefaultDocumentUpdateHandler implements DocumentUpdateHandler {
     }
 
     protected fireDocumentUpdate(changed: URI[], deleted: URI[]): void {
+        // Filter out URIs that do not have a service in the registry
+        // Running the document builder update will fail for those URIs
+        changed = changed.filter(uri => this.hasService(uri));
         // Only fire the document update when the workspace manager is ready
         // Otherwise, we might miss the initial indexing of the workspace
         this.workspaceManager.ready.then(() => {
@@ -81,6 +87,23 @@ export class DefaultDocumentUpdateHandler implements DocumentUpdateHandler {
             // This should never happen, but if it does, we want to know about it
             console.error('Workspace initialization failed. Could not perform document update.', err);
         });
+    }
+
+    /**
+     * Check whether the service registry contains a service instance for the given URI.
+     *
+     * Some language clients (vscode) decide to send update notifications for files that have been renamed to a different file extension.
+     * In this case, the service registry may not contain a service for the new URI. We have to ignore any changes to those files.
+     *
+     * In case we only have a single language in our registry, we can safely use that language for all URIs.
+     */
+    protected hasService(uri: URI): boolean {
+        try {
+            this.serviceRegistry.getServices(uri);
+            return true;
+        } catch {
+            return false;
+        }
     }
 
     didChangeContent(change: TextDocumentChangeEvent<TextDocument>): void {
