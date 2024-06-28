a85785d2462fa1302e1795a21a7ab80a489d456f
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2024-06-28 12:13:21 +0200

    Update packages/langium/src/lsp/document-symbol-provider.ts
    Co-authored-by: Mark Sujew <mark.sujew@typefox.io>

--- packages/langium/src/lsp/document-symbol-provider.ts
+++ packages/langium/src/lsp/document-symbol-provider.ts
@@ -48,7 +48,7 @@ export class DefaultDocumentSymbolProvider implements DocumentSymbolProvider {
             const name = this.nameProvider.getName(astNode);
             return [{
                 kind: this.nodeKindProvider.getSymbolKind(astNode),
-                name: name === undefined || name.length === 0 ? nameNode.text : name,
+                name: name || nameNode.text,
                 range: node.range,
                 selectionRange: nameNode.range,
                 children: this.getChildSymbols(document, astNode)
