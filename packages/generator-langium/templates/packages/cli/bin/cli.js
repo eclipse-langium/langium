782a12eee7fc36f7a8fdc4221f3a1405fc487df5
Kai Salmen <kai.salmen@typefox.io>
2024-07-08 13:33:42 +0200

    fix: correction to cli

--- packages/generator-langium/templates/packages/cli/bin/cli.js
+++ packages/generator-langium/templates/packages/cli/bin/cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
-import main from '../out/cli/main.js';
+import main from '../out/main.js';
 main();
--- packages/generator-langium/templates/packages/cli/src/main.ts
+++ packages/generator-langium/templates/packages/cli/src/main.ts
@@ -10,7 +10,7 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const packagePath = path.resolve(__dirname, '..', '..', 'package.json');
+const packagePath = path.resolve(__dirname, '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
