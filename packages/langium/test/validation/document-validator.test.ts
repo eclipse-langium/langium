507d4a8502069ae1c4ef19db775af36c11db246d
Johannes Meier <johannes.meier@typefox.io>
2024-07-30 12:29:56 +0200

    test cases for the new validation API

--- packages/langium/test/validation/document-validator.test.ts
+++ packages/langium/test/validation/document-validator.test.ts
@@ -4,12 +4,13 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
-import type { AstNode, ValidationChecks } from 'langium';
+import { AstUtils, type AstNode, type ValidationChecks } from 'langium';
 import type { ValidationResult } from 'langium/test';
 import { beforeAll, describe, expect, test } from 'vitest';
 import { Position, Range } from 'vscode-languageserver';
 import { createServicesForGrammar } from 'langium/grammar';
 import { validationHelper } from 'langium/test';
+import type { LangiumServices } from 'langium/lsp';
 
 // Related to https://github.com/eclipse-langium/langium/issues/571
 describe('Parser error is thrown on resynced token with NaN position', () => {
@@ -107,6 +108,145 @@ describe('Generic `AstNode` validation applies to all nodes', () => {
         expect(diagnostics.filter(d => d.severity === 2)).toHaveLength(3); // 3 warnings
         expect(diagnostics.filter(d => d.severity === 3)).toHaveLength(0);
         expect(diagnostics.filter(d => d.severity === 4)).toHaveLength(0);
+    });
+
+});
+
+describe('Register Before/AfterDocument logic for validations with state', () => {
+
+    const grammar = `grammar NestedNamedNodes
+
+    entry Model:
+        (elements+=NamedNode)*;
+
+    NamedNode:
+        name=ID '{' (children+=NamedNode)* '}';
+
+    hidden terminal WS: /\\s+/;
+    terminal ID: /[_a-zA-Z][\\w_]*/;
+    `;
+
+    let validate: (input: string) => Promise<ValidationResult<AstNode>>;
+    let services: LangiumServices;
+
+    function getFQN(node: AstNode): string {
+        if ('name' in node && node.$container) {
+            const parentName = getFQN(node.$container);
+            if (parentName) {
+                return parentName + '.' + node.name;
+            }
+            return '' + node.name;
+        }
+        return '';
+    }
+
+    function rememberNamedNode(child: AstNode, fqnMap: Map<string, AstNode[]>): void {
+        const fqn = getFQN(child);
+        let list = fqnMap.get(fqn);
+        if (!list) {
+            list = [];
+            fqnMap.set(fqn, list);
+        }
+        list.push(child);
+    }
+
+    function registerValidationForRootElement() {
+        const validationChecks: ValidationChecks<object> = {
+            // Note that there are no generated types like Model and NamedElement here
+            AstNode: (node, accept) => {
+                if ('name' in node) {
+                    // ignore nodes with names here
+                } else {
+                    // do the expensive validation once on the root node only (usually, this is registered like "Model: ..."!)
+                    const fqnMap = new Map<string, AstNode[]>();
+                    // collect the FQN of all nodes
+                    // !! This 'streamAllContents' is saved in the alternative version of the validation !!
+                    AstUtils.streamAllContents(node).forEach(child => {
+                        rememberNamedNode(child, fqnMap);
+                    });
+                    // check for duplicates
+                    for (const [key, value] of fqnMap.entries()) {
+                        if (value.length >= 2) {
+                            value.forEach(child => accept('error', `The FQN '${key}' is not unique.`, { node: child }));
+                        }
+                    }
+                }
+            },
+        };
+        services.validation.ValidationRegistry.register(validationChecks);
+    }
+
+    function registerValidationBeforeAfter() {
+        const fqnMap = new Map<string, AstNode[]>();
+        services.validation.ValidationRegistry.registerBeforeDocument((_rootNode, _accept, _categories) => {
+            // clear everything to be sure
+            fqnMap.clear();
+        });
+        const validationChecks: ValidationChecks<object> = {
+            AstNode: (node, _accept) => {
+                if ('name' in node) {
+                    // only register the named nodes in the map
+                    // Streaming the whole AST is not required with this approach, since the streaming is already done by the DocumentValidator!
+                    rememberNamedNode(node, fqnMap);
+                } else {
+                    // do nothing for the root node
+                }
+            },
+        };
+        services.validation.ValidationRegistry.register(validationChecks);
+        services.validation.ValidationRegistry.registerAfterDocument((_rootNode, accept, _categories) => {
+            // check for duplicates
+            for (const [key, value] of fqnMap.entries()) {
+                if (value.length >= 2) {
+                    value.forEach(child => accept('error', `The FQN '${key}' is not unique.`, { node: child }));
+                }
+            }
+            fqnMap.clear();
+        });
+    }
+
+    describe('Using the stateless validation', () => {
+        beforeAll(async () => {
+            services = await createServicesForGrammar({
+                grammar
+            });
+            validate = validationHelper(services);
+            registerValidationForRootElement();
+        });
+
+        test('Children with same name on same level (stateless)', async () => {
+            const validationResult = await validate('A { B{} C{} B{} }');
+            const diagnostics = validationResult.diagnostics;
+            expect(diagnostics).toHaveLength(2);
+        });
+
+        test('Nested Children with same name (stateless)', async () => {
+            const validationResult = await validate('A { A{ A{} } }');
+            const diagnostics = validationResult.diagnostics;
+            expect(diagnostics).toHaveLength(0);
+        });
+    });
+
+    describe('Using the validation with state', () => {
+        beforeAll(async () => {
+            services = await createServicesForGrammar({
+                grammar
+            });
+            validate = validationHelper(services);
+            registerValidationBeforeAfter();
+        });
+
+        test('Children with same name on same level (with state)', async () => {
+            const validationResult = await validate('A { B{} C{} B{} }');
+            const diagnostics = validationResult.diagnostics;
+            expect(diagnostics).toHaveLength(2);
+        });
+
+        test('Nested Children with same name (with state)', async () => {
+            const validationResult = await validate('A { A{ A{} } }');
+            const diagnostics = validationResult.diagnostics;
+            expect(diagnostics).toHaveLength(0);
+        });
     });
 
 });
