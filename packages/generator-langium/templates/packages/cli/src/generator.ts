09e17afb25ab9df5d26326640e58f0d2776dab48
Kai Salmen <kai.salmen@typefox.io>
2025-02-14 16:54:34 +0100

    Transform generated project to npm workspace
    - Align template structure with target structure
    - Integrate a global tsconfig.build.json
    - Unify how build and compile is done
    - Enhance tests
    - Updated relevant dependencies and aligned version in the repo
    - Move test output to packages/generator-langium/test/genetator-tests
    - Implement review comments: tmLanguage location and package names use languageId
    - Do not use "run --prefix" and align eslint+plugin version with typescript
    - Updated README

---
+++ packages/generator-langium/templates/packages/cli/src/generator.ts
@@ -0,0 +1,22 @@
+import type { Model } from '<%= language-id %>-language';
+import { expandToNode, joinToNode, toString } from 'langium/generate';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { extractDestinationAndName } from './util.js';
+
+export function generateJavaScript(model: Model, filePath: string, destination: string | undefined): string {
+    const data = extractDestinationAndName(filePath, destination);
+    const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
+
+    const fileNode = expandToNode`
+        "use strict";
+
+        ${joinToNode(model.greetings, greeting => `console.log('Hello, ${greeting.person.ref?.name}!');`, { appendNewLineIfNotEmpty: true })}
+    `.appendNewLineIfNotEmpty();
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(generatedFilePath, toString(fileNode));
+    return generatedFilePath;
+}
