a6d4692c66ac1ddb1be3491658b542d1ca97cd95
Kai Salmen <kai.salmen@typefox.io>
2025-02-14 16:54:34 +0100

    Transform generated project to npm workspace
    - Align template structure with target structure
    - Integrate a global tsconfig.build.json
    - Unify how build and compile is done
    - Enhance tests
    - Updated relevant dependencies and aligned version in the repo
    - Move test output to packages/generator-langium/test/genetator-tests
    - Implement review comments: tmLanguage location and package names use languageId
    - Do not use "run --prefix" and align eslint+plugin version with typescript
    - Updated READMEs
    - align vitest usage

---
+++ packages/generator-langium/templates/packages/extension/src/language/main.ts
@@ -0,0 +1,13 @@
+import { startLanguageServer } from 'langium/lsp';
+import { NodeFileSystem } from 'langium/node';
+import { createConnection, ProposedFeatures } from 'vscode-languageserver/node.js';
+import { create<%= LanguageName %>Services } from '<%= language-id %>-language';
+
+// Create a connection to the client
+const connection = createConnection(ProposedFeatures.all);
+
+// Inject the shared services and language-specific services
+const { shared } = create<%= LanguageName %>Services({ connection, ...NodeFileSystem });
+
+// Start the language server with the shared services
+startLanguageServer(shared);
