472b332fe497a9229c215bfe004becee78fa5d0d
Dennis Huebner <dennis.huebner@gmail.com>
2025-03-19 14:53:57 +0100

    Fragments, hidden terminals and comment handling.

--- packages/langium-cli/test/generator/bnf-generator.test.ts
+++ packages/langium-cli/test/generator/bnf-generator.test.ts
@@ -16,43 +16,123 @@ const { grammar } = createLangiumGrammarServices(EmptyFileSystem);
 
 describe('BNF generator', () => {
 
-    test('should generate GBNF file', async () => {
+    test('GBNF - Simple grammar', async () => {
         const result = (await parseHelper<Grammar>(grammar)(TEST_GRAMMAR)).parseResult;
         const typesFileContent = generateBnf([result.value]);
         expect(typesFileContent).toBe(EXPECTED_BNF);
     });
 
+    test('GBNF - Fragment, comment generation', async () => {
+        const result = (await parseHelper<Grammar>(grammar)(GRAMMAR_WITH_FRAGMENT)).parseResult;
+        const typesFileContent = generateBnf([result.value]);
+        const expected = expandToStringWithNL`
+        # * This is a root rule
+        root ::= package-declaration*
+
+        # * This is a package declaration * Using parser rule fragment
+        package-declaration ::= hidden* "package" named-element hidden* "{" hidden* "}"
+
+        named-element ::= qualified-name
+
+        qualified-name ::= id ( hidden* "." id )*
+
+        # * Terminal fragment
+        number ::= hidden* [0-9]+
+
+        # * Terminal using fragment
+        signed-number ::= hidden* [+-] number+
+
+        # Terminal rule
+        id ::= hidden* [A-z]*
+
+        ws ::= \\s+
+
+        hidden ::= ( ws )
+        `;
+        expect(typesFileContent).toBe(expected);
+    });
+
+    test('EBNF - Fragment, comment generation', async () => {
+        const result = (await parseHelper<Grammar>(grammar)(GRAMMAR_WITH_FRAGMENT)).parseResult;
+        const typesFileContent = generateBnf([result.value], { dialect: 'EBNF' });
+        const expected = expandToStringWithNL`
+        /* * This is a root rule */
+        <Domainmodel> ::= <PackageDeclaration>*
+
+        /* * This is a package declaration * Using parser rule fragment */
+        <PackageDeclaration> ::= <HIDDEN>* "package" <NamedElement> <HIDDEN>* "{" <HIDDEN>* "}"
+
+        <NamedElement> ::= <QualifiedName>
+
+        <QualifiedName> ::= <ID> ( <HIDDEN>* "." <ID> )*
+
+        /* * Terminal fragment */
+        <NUMBER> ::= <HIDDEN>* [0-9]+
+
+        /* * Terminal using fragment */
+        <SIGNED_NUMBER> ::= <HIDDEN>* [+-] <NUMBER>+
+
+        /* Terminal rule */
+        <ID> ::= <HIDDEN>* [A-z]*
+
+        <WS> ::= \\s+
+
+        <HIDDEN> ::= ( <WS> )
+        `;
+        expect(typesFileContent).toBe(expected);
+    });
+
+    test('GBNF - No hidden rules', async () => {
+        const grammarContent = `
+        grammar RootLang
+        entry Root:
+            'root' name=ID;
+        terminal ID: /[_a-zA-Z][\\w_]*/;
+        `;
+
+        const result = (await parseHelper<Grammar>(grammar)(grammarContent)).parseResult;
+        const typesFileContent = generateBnf([result.value]);
+        const expected = expandToStringWithNL`
+        root ::= "root" id
+
+        id ::= [_a-zA-Z][\\w_]*
+
+        `;
+        expect(typesFileContent).toBe(expected);
+    });
+
 });
 
 const EXPECTED_BNF = expandToStringWithNL`
-root ::= "module" ID Statement*
+root ::= hidden* "module" id statement*
 
-Statement ::= (Definition | Evaluation)
+statement ::= (definition | evaluation)
 
-Definition ::= "def" ID ("(" DeclaredParameter ("," DeclaredParameter)* ")")? ":" Expression ";"
+definition ::= hidden* "def" id ( hidden* "(" declared-parameter ( hidden* "," declared-parameter )* hidden* ")" )? hidden* ":" expression hidden* ";"
 
-DeclaredParameter ::= ID
+declared-parameter ::= id
 
-Evaluation ::= Expression ";"
+evaluation ::= expression hidden* ";"
 
-Expression ::= Addition
+expression ::= addition
 
-Addition ::= Multiplication (("+" | "-") Multiplication)*
+addition ::= multiplication ( (hidden* "+" | hidden* "-") multiplication )*
 
-Multiplication ::= PrimaryExpression (("*" | "/") PrimaryExpression)*
+multiplication ::= primary-expression ( (hidden* "*" | hidden* "/") primary-expression )*
 
-PrimaryExpression ::= ("(" Expression ")" | NUMBER | ID ("(" Expression ("," Expression)* ")")?)
+primary-expression ::= (hidden* "(" expression hidden* ")" | number | ID ( hidden* "(" expression ( hidden* "," expression )* hidden* ")" )?)
 
-WS ::= \\s+
+ws ::= \\s+
 
-ID ::= [_a-zA-Z][\\w_]*
+id ::= hidden* [_a-zA-Z][\\w_]*
 
-NUMBER ::= [0-9]+(\\.[0-9]*)?
+number ::= hidden* [0-9]+(\\.[0-9]*)?
 
-ML_COMMENT ::= /\\*[\\s\\S]*?\\*/
+ml-comment ::= /\\*[\\s\\S]*?\\*/
 
-SL_COMMENT ::= //[^\\n\\r]*
+sl-comment ::= //[^\\n\\r]*
 
+hidden ::= ( ws | ml-comment | sl-comment )
 `;
 
 const TEST_GRAMMAR = expandToStringWithNL`
@@ -97,4 +177,35 @@ const TEST_GRAMMAR = expandToStringWithNL`
 
     hidden terminal ML_COMMENT: /\\/\\*[\\s\\S]*?\\*\\//;
     hidden terminal SL_COMMENT: /\\/\\/[^\\n\\r]*/;
+`;
+
+const GRAMMAR_WITH_FRAGMENT = expandToStringWithNL`
+    grammar DomainModel
+    /** This is a root rule */
+    entry Domainmodel:
+        (elements+=PackageDeclaration)*;
+
+    /*
+    * This is a package declaration
+    * Using parser rule fragment
+    */
+    PackageDeclaration:
+        'package' NamedElement '{'
+        '}';
+
+    fragment NamedElement:
+        name=QualifiedName
+    ;
+
+    // Datatype rule
+    QualifiedName returns string:
+        ID ('.' ID)*;
+
+    /** Terminal fragment */
+    terminal fragment NUMBER: /[0-9]+/;
+    /** Terminal using fragment */
+    terminal SIGNED_NUMBER returns number: /[+-]/ NUMBER+;
+    /* Terminal rule */
+    terminal ID: /[A-z]*/;
+    hidden terminal WS: /\\s+/;
 `;
