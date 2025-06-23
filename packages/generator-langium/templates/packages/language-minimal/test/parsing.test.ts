217f381116f1146f9ed5d66244ae8038c7c3ba0e
Julian MAZAT <julian.mazat12@gmail.com>
2025-06-23 14:11:04 +0200

    Add setups & teardown in minimal template

--- packages/generator-langium/templates/packages/language-minimal/test/parsing.test.ts
+++ packages/generator-langium/templates/packages/language-minimal/test/parsing.test.ts
@@ -1,5 +1,19 @@
 import { describe } from "vitest";
 
+/*
+let services: ReturnType<typeof create<%= LanguageName %>Services>;
+let parse:    ReturnType<typeof parseHelper<<%= EntryName %>>>;
+let document: LangiumDocument<<%= EntryName %>> | undefined;
+
+beforeAll(async () => {
+    services = create<%= LanguageName %>Services(EmptyFileSystem);
+    parse = parseHelper<<%= EntryName %>>(services.<%= LanguageName %>);
+
+    // activate the following if your linking test requires elements from a built-in library, for example
+    // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
+});
+*/
+
 describe('Parsing tests', () => {
 
     // TODO: Add parsing tests
--- packages/generator-langium/templates/packages/language-minimal/test/validating.test.ts
+++ packages/generator-langium/templates/packages/language-minimal/test/validating.test.ts
@@ -1,5 +1,20 @@
 import { describe } from "vitest";
 
+/*
+let services: ReturnType<typeof create<%= LanguageName %>Services>;
+let parse:    ReturnType<typeof parseHelper<<%= EntryName %>>>;
+let document: LangiumDocument<<%= EntryName %>> | undefined;
+
+beforeAll(async () => {
+    services = create<%= LanguageName %>Services(EmptyFileSystem);
+    const doParse = parseHelper<<%= EntryName %>>(services.<%= LanguageName %>);
+    parse = (input: string) => doParse(input, { validation: true });
+
+    // activate the following if your linking test requires elements from a built-in library, for example
+    // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
+});
+*/
+
 describe('Validating', () => {
 
     // TODO: Add validation tests
