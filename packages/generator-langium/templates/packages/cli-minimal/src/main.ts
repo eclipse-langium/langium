1a3ce6957cac7b8bbea3931fa69ba48f1866d30f
Julian MAZAT <julian.mazat12@gmail.com>
2025-06-23 14:55:40 +0200

    Restore CLI generation in minimal project

--- packages/generator-langium/templates/packages/cli-minimal/src/main.ts
+++ packages/generator-langium/templates/packages/cli-minimal/src/main.ts
@@ -1,4 +1,10 @@
+import type { <%= EntryName %> } from '<%= language-id %>-language';
+import { create<%= LanguageName %>Services, <%= LanguageName %>LanguageMetaData } from '<%= language-id %>-language';
+import chalk from 'chalk';
 import { Command } from 'commander';
+import { extractAstNode } from './util.js';
+import { generateJavaScript } from './generator.js';
+import { NodeFileSystem } from 'langium/node';
 import * as url from 'node:url';
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
@@ -6,6 +12,13 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const packagePath = path.resolve(__dirname, '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
+
+export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
+    const services = create<%= LanguageName %>Services(NodeFileSystem).<%= LanguageName %>;
+    const model = await extractAstNode<<%= EntryName %>>(fileName, services);
+    const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
+    console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+};
 
 export type GenerateOptions = {
     destination?: string;
@@ -17,6 +30,13 @@ export default function(): void {
     program.version(JSON.parse(packageContent).version);
 
     // TODO: use Program API to declare the CLI
+    const fileExtensions = <%= LanguageName %>LanguageMetaData.fileExtensions.join(', ');
+    program
+        .command('generate')
+        .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
+        .option('-d, --destination <dir>', 'destination directory of generating')
+        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+        .action(generateAction);
 
     program.parse(process.argv);
 }
