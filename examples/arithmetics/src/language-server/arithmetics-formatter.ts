c76d53894f06941f5f97e63f3903c3d9e45b666c
Gregory Popov <tordektopor@gmail.com>
2025-09-19 18:59:19 +0100

    Elaborate formatting within statements
    Make function definitions two lines, while parameter definitions one line

--- examples/arithmetics/src/language-server/arithmetics-formatter.ts
+++ examples/arithmetics/src/language-server/arithmetics-formatter.ts
@@ -14,46 +14,52 @@ export class ArithmeticsFormatter extends AbstractFormatter {
             const formatter = this.getNodeFormatter(node);
             // Format module declaration: "module" keyword followed by space, then name
             formatter.keyword('module').append(Formatting.oneSpace());
+            formatter.property('name').append(Formatting.newLine());
             // All statements should be aligned to the root (no additional indentation)
             const statements = formatter.nodes(...node.statements);
             statements.prepend(Formatting.noIndent());
         } else if (ast.isDefinition(node)) {
             const formatter = this.getNodeFormatter(node);
-            // Format definition: "def" keyword followed by space
             formatter.keyword('def').append(Formatting.oneSpace());
-            // Space before colon and after colon
-            formatter.keyword(':').surround(Formatting.oneSpace());
-            // No space before semicolon
-            formatter.keyword(';').prepend(Formatting.noSpace());
-            // Format parentheses around parameters (if any)
+            formatter.keyword(':').prepend(Formatting.noSpace());
+
+            // Format Definition of a function
             if (node.args.length > 0) {
-                formatter.keyword('(').append(Formatting.noSpace());
-                formatter.keyword(')').prepend(Formatting.noSpace());
+                formatter.keywords('(', ')').surround(Formatting.noSpace());
                 // Space after commas in parameter lists
                 formatter.keywords(',').append(Formatting.oneSpace());
+                formatter.property('expr').prepend(Formatting.indent());
+            // Format Definition of a constant
+            } else {
+                formatter.property('expr').prepend(Formatting.oneSpace());
             }
         } else if (ast.isEvaluation(node)) {
             const formatter = this.getNodeFormatter(node);
             // No space before semicolon
             formatter.keyword(';').prepend(Formatting.noSpace());
-        } else if (ast.isBinaryExpression(node)) {
+        } else if (ast.isExpression(node)) {
             const formatter = this.getNodeFormatter(node);
-            // Spaces around all binary operators (+, -, *, /, %, ^)
-            formatter.keywords('+', '-', '*', '/', '%', '^').surround(Formatting.oneSpace());
+            formatter.keyword('(').append(Formatting.noSpace());
+            formatter.keyword(')').prepend(Formatting.noSpace());
+            if (ast.isBinaryExpression(node)) {
+                // const formatter = this.getNodeFormatter(node);
+                // Spaces around all binary operators
+                // TODO: Infix rules cannot be formatted
+                // operators cannot be formatted neither as keywords nor as properties
+                // left/right property cannot be formatted either
+            }
         } else if (ast.isFunctionCall(node)) {
             const formatter = this.getNodeFormatter(node);
             // Format parentheses and arguments (if any)
             if (node.args.length > 0) {
-                formatter.keyword('(').append(Formatting.noSpace());
-                formatter.keyword(')').prepend(Formatting.noSpace());
+                formatter.keywords('(', ')').surround(Formatting.noSpace());
                 // Space after commas in argument lists
                 formatter.keywords(',').append(Formatting.oneSpace());
             }
         }
 
-        // Handle parentheses in expressions - no space inside parentheses
+        // No space around semicolons in all cases
         const formatter = this.getNodeFormatter(node);
-        formatter.keyword('(').append(Formatting.noSpace());
-        formatter.keyword(')').prepend(Formatting.noSpace());
+        formatter.keyword(';').surround(Formatting.noSpace());
     }
 }
