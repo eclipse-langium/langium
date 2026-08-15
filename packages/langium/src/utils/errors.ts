3d6b69939936e0d260a2997b6a369b8a621edd90
Johannes Meier <johannes.meier@typefox.io>
2026-08-15 14:44:36 +0200

    extended existing assertions to accept error messages, which are calculated on-demand

--- packages/langium/src/utils/errors.ts
+++ packages/langium/src/utils/errors.ts
@@ -7,17 +7,18 @@
 import type { CstNode } from '../syntax-tree.js';
 
 export class ErrorWithLocation extends Error {
-    constructor(node: CstNode | undefined, message: string) {
-        super(node ? `${message} at ${node.range.start.line}:${node.range.start.character}` : message);
+    constructor(node: CstNode | undefined, message: string | (() => string)) {
+        const msg = typeof message === 'string' ? message : message();
+        super(node ? `${msg} at ${node.range.start.line}:${node.range.start.character}` : msg);
     }
 }
 
-export function assertUnreachable(_: never, message = 'Error: Got unexpected value.'): never {
-    throw new Error(message);
+export function assertUnreachable(_: never, message: string | (() => string) = 'Error: Got unexpected value.'): never {
+    throw new Error(typeof message === 'string' ? message : message());
 }
 
-export function assertCondition(condition: boolean, message: string = 'Error: Condition is violated.'): asserts condition {
+export function assertCondition(condition: boolean, message: string | (() => string) = 'Error: Condition is violated.'): asserts condition {
     if (!condition) {
-        throw new Error(message);
+        throw new Error(typeof message === 'string' ? message : message());
     }
 }
