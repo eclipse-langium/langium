4210ca667ad41d903c704bcc9c476d135901dee8
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2026-08-25 15:47:46 +0200

    chore: add npm script whitelist in preparation for npm 12 (#2212)
    * add npm script whitelist in preparation for npm 12
    
    * also allow in generator and use exact same version in both
    
    * esbuild bump

--- packages/generator-langium/test/yeoman-generator.test.ts
+++ packages/generator-langium/test/yeoman-generator.test.ts
@@ -302,7 +302,11 @@ const PACKAGE_JSON_EXPECTATION: Record<string, any> = {
     },
     workspaces: [
         'packages/language'
-    ]
+    ],
+    allowScripts: {
+        'esbuild@0.28.2': true,
+        'fsevents@2.3.3': true
+    }
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -376,7 +380,7 @@ const PACKAGE_JSON_EXPECTATION_EXTENSION: Record<string, any> = {
     devDependencies: {
         '@types/vscode': '~1.91.0',
         'concurrently': '~10.0.4',
-        'esbuild': '~0.28.1'
+        'esbuild': '0.28.2'
     }
 };
 
